de04d4ef73b27c7620961211e6accb7c95de535a
Julius Härtl <jus@bitgrid.net>
2020-01-24 17:01:14 +0100

    Disable resizing on iOS in direct editing
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/views/DirectEditing.vue
+++ src/views/DirectEditing.vue
@@ -103,6 +103,7 @@ export default {
 	},
 	beforeMount() {
 		callMobileMessage('loading')
+		document.querySelector('meta[name="viewport"]').setAttribute('content', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0')
 	},
 	methods: {
 		async close() {
