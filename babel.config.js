40eb0939c1e8ce55bd0472b4c1d40dda6696bb53
Julius Härtl <jus@bitgrid.net>
2020-09-09 07:03:42 +0200

    Use core-js and babel plugins
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-	plugins: ['@babel/plugin-syntax-dynamic-import'],
+	plugins: [
+		'@babel/plugin-syntax-dynamic-import',
+		'@babel/plugin-transform-classes',
+		['@babel/plugin-proposal-class-properties', { loose: true }],
+	],
 	presets: [
 		[
 			'@babel/preset-env',
 			{
-				"modules": false
-			}
-		]
+				corejs: 3,
+				useBuiltIns: 'entry',
+			},
+		],
 	],
 	env: {
 		test: {
