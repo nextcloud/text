0ef40612008d82dcdfcab5befe24ed45ec4c18a8
Julius Härtl <jus@bitgrid.net>
2020-09-29 13:08:51 +0200

    Bump bundles
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- js/highlight/ldif.js
+++ js/highlight/ldif.js
@@ -1,2 +1,2 @@
-(window.textWebpackJsonp=window.textWebpackJsonp||[]).push([[92],{467:function(e,n){e.exports=function(e){return{contains:[{className:"attribute",begin:"^dn",end:": ",excludeEnd:!0,starts:{end:"$",relevance:0},relevance:10},{className:"attribute",begin:"^\\w",end:": ",excludeEnd:!0,starts:{end:"$",relevance:0}},{className:"literal",begin:"^-",end:"$"},e.HASH_COMMENT_MODE]}}}}]);
-//# sourceMappingURL=ldif.js.map?v=9800ce2d5ac13bbada54
+(window.textWebpackJsonp=window.textWebpackJsonp||[]).push([[92],{469:function(e,n){e.exports=function(e){return{contains:[{className:"attribute",begin:"^dn",end:": ",excludeEnd:!0,starts:{end:"$",relevance:0},relevance:10},{className:"attribute",begin:"^\\w",end:": ",excludeEnd:!0,starts:{end:"$",relevance:0}},{className:"literal",begin:"^-",end:"$"},e.HASH_COMMENT_MODE]}}}}]);
+//# sourceMappingURL=ldif.js.map?v=2848bd37974b251dfa29
