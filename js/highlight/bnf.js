be42f1b43a62f61a30b26d3cccb92dfe55a69d6b
Julius Härtl <jus@bitgrid.net>
2020-11-09 11:38:19 +0100

    Bump bundles
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- js/highlight/bnf.js
+++ js/highlight/bnf.js
@@ -1,2 +1,2 @@
-(window.textWebpackJsonp=window.textWebpackJsonp||[]).push([[22],{247:function(n,t){n.exports=function(n){return{contains:[{className:"attribute",begin:/</,end:/>/},{begin:/::=/,starts:{end:/$/,contains:[{begin:/</,end:/>/},n.C_LINE_COMMENT_MODE,n.C_BLOCK_COMMENT_MODE,n.APOS_STRING_MODE,n.QUOTE_STRING_MODE]}}]}}}}]);
-//# sourceMappingURL=bnf.js.map?v=bdbe9fec5f261b626972
+(window.textWebpackJsonp=window.textWebpackJsonp||[]).push([[22],{259:function(n,t){n.exports=function(n){return{contains:[{className:"attribute",begin:/</,end:/>/},{begin:/::=/,starts:{end:/$/,contains:[{begin:/</,end:/>/},n.C_LINE_COMMENT_MODE,n.C_BLOCK_COMMENT_MODE,n.APOS_STRING_MODE,n.QUOTE_STRING_MODE]}}]}}}}]);
+//# sourceMappingURL=bnf.js.map?v=e5b2418f120c4e1aa594
