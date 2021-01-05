5d8d0c73a95bedc32ae80af4509585a294a43366
Christoph Wurst <christoph@winzerhof-wurst.at>
2021-01-05 17:48:19 +0100

    Fix dbal types
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- lib/Migration/Version030201Date20201116123153.php
+++ lib/Migration/Version030201Date20201116123153.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace OCA\Text\Migration;
 
 use Closure;
-use Doctrine\DBAL\Types\Type;
+use Doctrine\DBAL\Types\Types;
 use OCP\DB\ISchemaWrapper;
 use OCP\IConfig;
 use OCP\Migration\IOutput;
@@ -31,32 +31,32 @@ class Version030201Date20201116123153 extends SimpleMigrationStep {
 		if (!$schema->hasTable('text_documents')) {
 			// Recreate table from the first migration since we cannot alter the autoincrement on the id column with oracle
 			$table = $schema->createTable('text_documents');
-			$table->addColumn('id', Type::BIGINT, [
+			$table->addColumn('id', Types::BIGINT, [
 				'notnull' => true,
 				'unsigned' => true,
 			]);
-			$table->addColumn('current_version', Type::BIGINT, [
+			$table->addColumn('current_version', Types::BIGINT, [
 				// 'notnull' => true,
 				'notnull' => false,
 				'default' => 0,
 				'unsigned' => true,
 			]);
-			$table->addColumn('last_saved_version', Type::BIGINT, [
+			$table->addColumn('last_saved_version', Types::BIGINT, [
 				// 'notnull' => true,
 				'notnull' => false,
 				'default' => 0,
 				'unsigned' => true,
 			]);
-			$table->addColumn('last_saved_version_time', Type::BIGINT, [
+			$table->addColumn('last_saved_version_time', Types::BIGINT, [
 				'length' => 20,
 				'unsigned' => true,
 			]);
-			$table->addColumn('last_saved_version_etag', Type::STRING, [
+			$table->addColumn('last_saved_version_etag', Types::STRING, [
 				'notnull' => false,
 				'length' => 64,
 				'default' => ''
 			]);
-			$table->addColumn('base_version_etag', Type::STRING, [
+			$table->addColumn('base_version_etag', Types::STRING, [
 				'notnull' => false,
 				'length' => 64,
 				'default' => ''
