73e97b1a560767960e3dc62237cca5a6d5fa744a
Julius Härtl <jus@bitgrid.net>
2021-06-29 15:21:22 +0200

    Log exceptions that happen on unknown exception and return generic error messages
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- lib/Service/ApiService.php
+++ lib/Service/ApiService.php
@@ -95,7 +95,8 @@ class ApiService {
 
 			$document = $this->documentService->createDocument($file);
 		} catch (Exception $e) {
-			return new DataResponse($e->getMessage(), 500);
+			$this->logger->logException($e);
+			return new DataResponse('Failed to create the document session', 500);
 		}
 
 		$session = $this->sessionService->initSession($document->getId(), $guestName);
@@ -187,9 +188,9 @@ class ApiService {
 		} catch (NotFoundException $e) {
 			return new DataResponse([], 404);
 		} catch (Exception $e) {
-			$this->logger->logException($e, ['level' => ILogger::INFO]);
+			$this->logger->logException($e);
 			return new DataResponse([
-				'message' => $e->getMessage()
+				'message' => 'Failed to autosave document'
 			], 500);
 		}
 
