34bc45f5da5c4ceb6df5134ec7450156389cebdd
Julius Härtl <jus@bitgrid.net>
2021-08-25 22:19:58 +0200

    Perform patch updates
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- js/highlight/erb.js
+++ js/highlight/erb.js
@@ -1,2 +1,2 @@
-(window.textWebpackJsonp=window.textWebpackJsonp||[]).push([[55],{494:function(n,e,u){"use strict";n.exports=function(n){return{name:"ERB",subLanguage:"xml",contains:[n.COMMENT("<%#","%>"),{begin:"<%[%=-]?",end:"[%-]?%>",subLanguage:"ruby",excludeBegin:!0,excludeEnd:!0}]}}}}]);
-//# sourceMappingURL=erb.js.map?v=5ab43af30f8d3f8c1dfe
+(window.textWebpackJsonp=window.textWebpackJsonp||[]).push([[55],{509:function(n,e,u){"use strict";n.exports=function(n){return{name:"ERB",subLanguage:"xml",contains:[n.COMMENT("<%#","%>"),{begin:"<%[%=-]?",end:"[%-]?%>",subLanguage:"ruby",excludeBegin:!0,excludeEnd:!0}]}}}}]);
+//# sourceMappingURL=erb.js.map?v=40bd9288cc8caa17c6dc
