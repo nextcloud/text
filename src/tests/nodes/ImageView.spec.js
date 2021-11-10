72fe3c6cb8f8dabd4efaf67c5c8a94216bdea7f7
Azul <azul@riseup.net>
2021-11-10 16:00:00 +0100

    fix: handling of preview urls and tests
    Preserve urls that start with `core/preview` no matter if `hasPreview` is in there or not.
    
    Also do not add a duplicate `/` in dav urls.
    `this.filePath` already starts with a `/`.
    
    Add a test for dav paths and fix some others that were broken by #1800.
    
    Signed-off-by: Azul <azul@riseup.net>

--- src/tests/nodes/ImageView.spec.js
+++ src/tests/nodes/ImageView.spec.js
@@ -36,17 +36,26 @@ describe('Image View src attribute based on markdown', () => {
 	})
 
 	test('fileId is used for preview url', () => {
-		const wrapper = factory({src: '/Media/photo-1498855592392-af2bf1e0a4c7.jpeg?fileId=7#mimetype=image%2Fjpeg&hasPreview=true'})
+		const src = '/Media/photo.jpeg?fileId=7#mimetype=image%2Fjpeg&hasPreview=true'
+		const wrapper = factory({src})
 		expect(wrapper.vm.fileId).toBe('7')
 		expect(wrapper.find('.image__main').attributes('src'))
-			.toBe('/core/preview?fileId=7&x=1024&y=1024&a=true')
+			.toContain('/core/preview?fileId=7')
+	})
+
+	test('use dav paths for gifs so they are animated', () => {
+		const src = '/Media/giffy.gif?fileId=7#mimetype=image%2Fgif&hasPreview=true'
+		const wrapper = factory({src})
+		expect(wrapper.vm.options.currentDirectory).toBe('/current')
+		expect(wrapper.find('.image__main').attributes('src'))
+			.toContain("remote.php/dav/files/user1/current/Media/giffy.gif")
 	})
 
 	test('without fileId relative path is used in file based preview url', () => {
-		const wrapper = factory({src: 'sub/asdf.jpg'})
+		const wrapper = factory({src: 'sub/asdf.jpg?hasPreview=true'})
 		expect(wrapper.vm.isSupportedImage).toBe(true)
 		expect(wrapper.find('.image__main').attributes('src'))
-			.toBe('/core/preview.png?file=%2Fcurrent%2Fsub%2Fasdf.jpg&x=1024&y=1024&a=true')
+			.toBe('/core/preview?file=%2Fcurrent%2Fsub%2Fasdf.jpg&x=1024&y=1024&a=true')
 	})
 
 	test('public share link previews are just used as they are', () => {
