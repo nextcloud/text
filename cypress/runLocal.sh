9a07509183d2c368a77c771a44cb0884e9d7b3b5
Azul <azul@riseup.net>
2022-01-04 17:23:47 +0100

    fix: 2020 let heading menu overflow workspace
    * roll back parts of #1903 that broke it again.
    * Calculate top of menububble based on scrollHeight of content-wrapper.
    * Always display menububble below selected line.
      This will make it less likely to conflict with the menubar
      or mobile copy and paste toolbars.
    * Add cypress tests for the workspace.
    
    Signed-off-by: Azul <azul@riseup.net>

--- cypress/runLocal.sh
+++ cypress/runLocal.sh
@@ -16,13 +16,21 @@ then
 fi
 
 # start server if it's not running yet
-if $(npm bin)/wait-on -i 500 -t 1000 $CYPRESS_baseUrl
+if $(npm bin)/wait-on -i 500 -t 1000 $CYPRESS_baseUrl 2> /dev/null
 then
 	echo Server is up at $CYPRESS_baseUrl
 else
+	echo No server reached at $CYPRESS_baseUrl - starting containers.
 	docker-compose up -d
-	$(npm bin)/wait-on -i 500 -t 240000 $CYPRESS_baseUrl || ( docker-compose logs ; exit 1 )
-	docker-compose exec -T nextcloud bash /var/www/html/apps/text/cypress/server.sh
+	if $(npm bin)/wait-on -i 500 -t 240000 $CYPRESS_baseUrl 2> /dev/null
+	then
+		docker-compose exec -T nextcloud bash /var/www/html/apps/text/cypress/server.sh
+	else
+		echo Waiting for $CYPRESS_baseUrl timed out.
+		echo Container logs:
+		docker-compose logs
+		exit 1
+	fi
 fi
 
 (cd .. && $(npm bin)/cypress $@)
