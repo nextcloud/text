54d6b790dc6b5da189541d99fd4cf2075d624212
Vinicius Reis <vinicius.reis@nextcloud.com>
2022-03-04 11:21:16 -0300

    📄 (2184): add missing comments
    Signed-off-by: Vinicius Reis <vinicius.reis@nextcloud.com>

--- src/markdownit/containers.js
+++ src/markdownit/containers.js
@@ -1,11 +1,33 @@
+/*
+ * @copyright Copyright (c) 2022 Vinicius Reis <vinicius@nextcloud.com>
+ *
+ * @author Vinicius Reis <vinicius@nextcloud.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import container from 'markdown-it-container'
 
 export const typesAvailable = ['info', 'warn', 'error', 'success']
 
 const buildRender = type => (tokens, idx, options, env, slf) => {
-	// add a class to the opening tag
 	const tag = tokens[idx]
 
+	// add attributes to the opening tag
 	if (tag.nesting === 1) {
 		tag.attrSet('data-container', type)
 		tag.attrJoin('class', `custom-container custom-container-${type}`)
@@ -14,7 +36,11 @@ const buildRender = type => (tokens, idx, options, env, slf) => {
 	return slf.renderToken(tokens, idx, options, env, slf)
 }
 
+/**
+ * @param {object} md Markdown object
+ */
 export default (md) => {
+	// create a custom container to each type
 	typesAvailable.forEach(type => {
 		md.use(container, type, {
 			render: buildRender(type),
