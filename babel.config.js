74e80da0fd98155bb72382e91abf9bc407d4e99f
Max <max@nextcloud.com>
2022-04-05 14:40:53 +0200

    build: add alias to babel config
    Signed-off-by: Max <max@nextcloud.com>

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
 const babelConfig = require('@nextcloud/babel-config')
 
+const resolvePlugin = ["module-resolver", {
+	"alias": {
+		"@nextcloud/text-editor": "./package",
+	}
+}]
+
 babelConfig.presets[0][1].modules = 'auto'
+babelConfig.plugins = [
+	...babelConfig.plugins,
+	resolvePlugin,
+]
 
 module.exports = babelConfig
