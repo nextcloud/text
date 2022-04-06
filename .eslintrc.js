505ab882939e2ae855611e8db44de23724df3a27
Max <max@nextcloud.com>
2022-04-06 16:25:31 +0200

    fix: use webpack resolver and turn off node/no-missing-imports
    eslint-plugin-import will use webpack resolver and check for missing imports.
    The node variant is causing false error reports as it does not use resolvers.
    
    See https://github.com/mysticatea/eslint-plugin-node/issues/249#issuecomment-747980937 for details.
    
    Signed-off-by: Max <max@nextcloud.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,16 @@ module.exports = {
 	],
 	settings: {
 		'import/resolver': {
-			webpack: {
-				config: 'webpack.js'
-			},
-			node: {
-				paths: ['src'],
-				extensions: ['.js', '.vue'],
-			},
+			webpack: { config: './webpack.js' },
 		},
+	},
+	rules: {
+		/* Turn off node checks for import resolution.
+		 * Let eslint-plugin-import handle this.
+		 * It knows how to use resolvers.
+		 *
+		 * See https://github.com/mysticatea/eslint-plugin-node/issues/249#issuecomment-747980937
+		 */
+		'node/no-missing-import': 'off',
 	},
 }
