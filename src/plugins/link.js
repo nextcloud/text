ff2e7aa2bbe331133209a6b07d444cd9b8825ec9
Max <max@nextcloud.com>
2022-04-14 16:56:10 +0200

    fix: find link href based on prosemirror node
    * Resolve the position given to the `handleClick` function.
    * Get the `link` mark from the resolved position.
    * Use the attrs of this mark.
    
    `editor.getAttributes` gets the attributes at the current selection.
    In Firefox the selection will not move to the clicked position in read only mode.
    So instead of relying on the selection we now use the `pos` of the click.
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/plugins/link.js
+++ src/plugins/link.js
@@ -5,12 +5,17 @@ const clickHandler = ({ editor, type, onClick }) => {
 		props: {
 			key: new PluginKey('textLink'),
 			handleClick: (view, pos, event) => {
-				const attrs = editor.getAttributes(type)
-				const link = event.target.closest('a')
-				if (link && attrs.href && onClick) {
-					return onClick(event, attrs)
+				const $clicked = view.state.doc.resolve(pos)
+				const link = $clicked.marks().find(m => m.type.name === type.name)
+				if (!link) {
+					return false
 				}
-				return false
+				if (!link.attrs.href) {
+					console.warn('Could not determine href of link.')
+					console.debug(link)
+					return false
+				}
+				return onClick?.(event, link.attrs)
 			},
 		},
 	})
