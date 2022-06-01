9a8f7613e733baa44eacc5cd651efe369e6d5906
Max <max@nextcloud.com>
2022-06-01 12:04:01 +0200

    vite: use vite-plugin-commonjs for vue-material-design-icons
    Signed-off-by: Max <max@nextcloud.com>

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { createVuePlugin } from 'vite-plugin-vue2'
+import commonjs from 'vite-plugin-commonjs'
 import { dependencies } from './package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [createVuePlugin()],
+	plugins: [createVuePlugin(), commonjs],
 	build: {
 		lib: {
 			entry: resolve(__dirname, 'src/package.js'),
