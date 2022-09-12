95c5e7c678df6ca4e5776a7dfe831ed695a0a5a8
Ferdinand Thiessen <rpm@fthiessen.de>
2022-09-12 20:35:54 +0200

    Add visible and clickable links for the page anchors
    Handle inter-page links properly and use a heading view for displaying links and link anchors. Implements #2173
    
    Also fix link handling, to scroll to selected header when
    clicking on an anchor link (`#some-id`).
    
    Signed-off-by: Ferdinand Thiessen <rpm@fthiessen.de>

--- src/nodes/Heading/index.js
+++ src/nodes/Heading/index.js
@@ -1,6 +1,8 @@
 import TipTapHeading from '@tiptap/extension-heading'
+import { VueNodeViewRenderer } from '@tiptap/vue-2'
 import debounce from 'debounce'
 import { setHeadings, extractHeadings } from './extractor.js'
+import HeaderViewVue from './HeadingView.vue'
 
 const Heading = TipTapHeading.extend({
 	addAttributes() {
@@ -16,11 +18,31 @@ const Heading = TipTapHeading.extend({
 			},
 		}
 	},
+
+	addOptions() {
+		return {
+			...this.parent?.(),
+			linkSymbol: '#',
+		}
+	},
+
 	addKeyboardShortcuts() {
 		return this.options.levels.reduce((items, level) => ({
 			...items,
 			[`Mod-Shift-${level}`]: () => this.editor.commands.toggleHeading({ level }),
 		}), {})
+	},
+
+	addNodeView() {
+		return VueNodeViewRenderer(HeaderViewVue, {
+			update: ({ oldNode, newNode, updateProps }) => {
+				if (newNode.type.name !== this.name) return false
+				// Make sure to redraw node as the vue renderer will not show the updated children
+				if (newNode.attrs !== oldNode.attrs) return false
+				updateProps()
+				return true
+			},
+		})
 	},
 
 	onDestroy() {
