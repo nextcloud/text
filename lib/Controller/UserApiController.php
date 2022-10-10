eef818985bcb94572f680c2020c22184391c6d3c
Jonas <jonas@freesources.org>
2022-10-10 16:02:42 +0200

    Remove duplicate assignment of sessionService in UserApiController
    Signed-off-by: Jonas <jonas@freesources.org>

--- lib/Controller/UserApiController.php
+++ lib/Controller/UserApiController.php
@@ -24,7 +24,6 @@ class UserApiController extends ApiController {
 		$this->collaboratorSearch = $ISearch;
 		$this->userSession = $userSession;
 		$this->userManager = $userManager;
-		$this->sessionService = $sessionService;
 	}
 
 	/**
