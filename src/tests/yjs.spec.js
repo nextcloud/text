2ecc23a15c195000761358f0c3f10090b651155b
Max <max@nextcloud.com>
2022-10-11 12:14:25 +0200

    analyse: recordings of editing session
    When playing back the editing session to a simulated guest
    they will reply with 500kb worth of SyncStep2 messages.
    
    This is the main factor that grows our database storage so quickly.
    
    Signed-off-by: Max <max@nextcloud.com>

---
+++ src/tests/yjs.spec.js
@@ -0,0 +1,126 @@
+import recorded from './fixtures/recorded'
+import { Doc, encodeStateAsUpdate } from 'yjs'
+import { decodeArrayBuffer } from '../helpers/base64.js'
+import * as decoding from 'lib0/decoding'
+import * as encoding from 'lib0/encoding'
+import * as syncProtocol from 'y-protocols/sync'
+
+describe('recorded session', () => {
+	const flattened = recorded.flat()
+	const sync = flattened.filter(step => step.startsWith('AA'))
+	const awareness = flattened.filter(step => /^A[QRSTU]/.test(step))
+	function size(arr) {
+		return arr.reduce((total, cur) => total + cur.length, 0)
+	}
+
+	test('original size', () => {
+		expect(recorded.length).toBe(4964)
+	})
+
+	test('flattened size', () => {
+		expect(flattened.length).toBe(8117)
+	})
+
+	test('sync & awareness count', () => {
+		expect(sync.length).toBe(2834)
+		expect(awareness.length).toBe(8117 - 2834)
+	})
+
+	test('sync & awareness size', () => {
+		expect(size(sync)).toBe(4710164)
+		expect(size(awareness)).toBe(87764)
+	})
+
+	test('message types', () => {
+		sync.forEach((step) => {
+			const buf = decodeArrayBuffer(step)
+			const decoder = decoding.createDecoder(buf)
+			const messageType = decoding.readVarUint(decoder)
+			const syncMessageType = decoding.peekVarUint(decoder)
+			expect(messageType).toBe(0)
+			expect([0, 1, 2]).toContain(syncMessageType)
+		})
+		awareness.forEach((step) => {
+			const buf = decodeArrayBuffer(step)
+			const decoder = decoding.createDecoder(buf)
+			const messageType = decoding.readVarUint(decoder)
+			expect(messageType).toBe(1)
+		})
+	})
+
+	test('sync messages types', () => {
+		const syncsOfType = type => sync.filter((step) => {
+			const buf = decodeArrayBuffer(step)
+			const decoder = decoding.createDecoder(buf)
+			const messageType = decoding.readVarUint(decoder)
+			return decoding.peekVarUint(decoder) === type
+		})
+		const step1s = syncsOfType(syncProtocol.messageYjsSyncStep1)
+		const step2s = syncsOfType(syncProtocol.messageYjsSyncStep2)
+		const updates = syncsOfType(syncProtocol.messageYjsUpdate)
+		expect(step1s.length).toBe(344)
+		expect(step2s.length).toBe(702)
+		expect(updates.length).toBe(1788)
+		expect(size(step1s)).toBe(41056)
+		expect(size(step2s)).toBe(4566084)
+		expect(size(updates)).toBe(103024)
+	})
+
+	test('read messages', () => {
+		const ydoc = new Doc()
+		sync.forEach((step) => {
+			const buf = decodeArrayBuffer(step)
+			const decoder = decoding.createDecoder(buf)
+			const encoder = encoding.createEncoder()
+			const messageType = decoding.readVarUint(decoder)
+			const inType = syncProtocol.readSyncMessage(
+				decoder,
+				encoder,
+				ydoc
+			)
+			expect([0, 1, 2]).toContain(inType)
+			if (inType !== syncProtocol.messageYjsSyncStep1) {
+				expect(encoding.length(encoder)).toBe(0)
+			}
+		})
+		expect(encodeStateAsUpdate(ydoc).length).toBe(34503)
+	})
+
+	test('analyse responses', () => {
+		const ydoc = new Doc()
+		const responses = sync.map((step) => {
+			const buf = decodeArrayBuffer(step)
+			const decoder = decoding.createDecoder(buf)
+			const encoder = encoding.createEncoder()
+			const messageType = decoding.readVarUint(decoder)
+			const inType = syncProtocol.readSyncMessage(
+				decoder,
+				encoder,
+				ydoc
+			)
+			if (!encoding.length(encoder)) {
+				return {inType}
+			}
+			const binary = encoding.toUint8Array(encoder)
+			const outBuf = decodeArrayBuffer(binary)
+			const outDecoder = decoding.createDecoder(outBuf)
+			const outType = decoding.peekVarUint(outDecoder)
+			return {inType, outType, length: encoding.length(encoder)}
+		})
+		responses.forEach(({inType, outType}) => {
+			if (inType === syncProtocol.messageYjsSyncStep1) {
+				expect(outType).toBe(syncProtocol.messageYjsSyncStep2)
+			}
+			if (inType === syncProtocol.messageYjsSyncStep2) {
+				expect(outType).toBeUndefined()
+			}
+			if (inType === syncProtocol.messageYjsUpdate) {
+				expect(outType).toBeUndefined()
+			}
+		})
+		const replies = responses.filter(r => r.length)
+		expect(replies.length).toBe(344)
+		expect(size(replies)).toBe(562868)
+	})
+
+})
