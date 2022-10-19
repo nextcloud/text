eb20f31941f4e7a44e96dddf8b4b589e99cb9e17
Jonas <jonas@freesources.org>
2022-10-19 12:57:31 +0200

    Make images block elements
    Use markdown-it-image-figures to render standalone images inside
    `<figure>` elements instead of `<p>` when markdown source gets parsed
    into HTML. This allows to treat images as block elements by TipTap.
    
    Also keep support for inline images to not break markdown files that
    have inline images.
    
    Fixes: #2873
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/markdownit/index.js
+++ src/markdownit/index.js
@@ -5,6 +5,7 @@ import underline from './underline.js'
 import splitMixedLists from './splitMixedLists.js'
 import callouts from './callouts.js'
 import keepSyntax from './keepSyntax.js'
+import implicitFigures from 'markdown-it-image-figures'
 
 const markdownit = MarkdownIt('commonmark', { html: false, breaks: false })
 	.enable('strikethrough')
@@ -15,6 +16,7 @@ const markdownit = MarkdownIt('commonmark', { html: false, breaks: false })
 	.use(callouts)
 	.use(keepSyntax)
 	.use(markdownitMentions)
+	.use(implicitFigures)
 
 // Issue #3370: To preserve softbreaks within md files we preserve all whitespaces, so we must not introduce additional new lines after a <br> element
 markdownit.renderer.rules.hardbreak = (tokens, idx, options) => (options.xhtmlOut ? '<br />' : '<br>')
