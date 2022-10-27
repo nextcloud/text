e78f78f2e9f6214c22b7631090f92eaccbf3d2bb
Jonas <jonas@freesources.org>
2022-10-27 18:49:27 +0200

    Remove trailing newline from code blocks
    Markdown-it serialized code blocks to `<pre><code>...\n</pre></code>`,
    which leads to trailing newlines in the code blocks as rendered by
     Tiptap.
    
    Let the Tiptap CodeBlock node remove the trailing newline in parseHTML
    in order to avoid this.
    
    There's another issue with trailing newlines that got added to code
    blocks on purpose being removed by prosemirror-markdown, but that's not
    affected by this change.
    
    Fixes: #2344
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/nodes/CodeBlock.js
+++ src/nodes/CodeBlock.js
@@ -2,6 +2,18 @@ import TiptapCodeBlock from '@tiptap/extension-code-block'
 import { defaultMarkdownSerializer } from 'prosemirror-markdown'
 
 const CodeBlock = TiptapCodeBlock.extend({
+	parseHTML() {
+		return [
+			{
+				tag: 'pre',
+				preserveWhitespace: 'full',
+				// Remove trailing newline from code blocks (Github issue #2344)
+				getContent: (node, schema) => {
+					return schema.nodes.codeBlock.create(null, [schema.text(node.textContent.replace(/\n$/, ''))])
+				},
+			},
+		]
+	},
 
 	toMarkdown(state, node, parent, index) {
 		// prosemirror-markdown uses `params` instead of `language` attribute
