c4ffc12ffc3e511c9321c7d0ec3470d2f6b9baa8
nextcloud-command <nextcloud-command@users.noreply.github.com>
2023-02-08 11:24:26 +0000

    Compile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/highlight/diff-js-js.js
+++ js/highlight/diff-js-js.js
@@ -1,2 +1,2 @@
-(self.webpackChunk_nextcloud_text=self.webpackChunk_nextcloud_text||[]).push([["highlight/diff-js-js"],{86088:(e,i,n)=>{var s=n(25108);!function e(){e.warned||(e.warned=!0,s.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/diff" instead of "highlight.js/lib/languages/diff.js"'))}(),e.exports=n(87731)}}]);
-//# sourceMappingURL=diff-js-js.js.map?v=efee96d9a7189b728608
+(self.webpackChunk_nextcloud_text=self.webpackChunk_nextcloud_text||[]).push([["highlight/diff-js-js"],{11612:(e,i,n)=>{var s=n(25108);!function e(){e.warned||(e.warned=!0,s.log('Deprecation (warning): Using file extension in specifier is deprecated, use "highlight.js/lib/languages/diff" instead of "highlight.js/lib/languages/diff.js"'))}(),e.exports=n(87731)}}]);
+//# sourceMappingURL=diff-js-js.js.map?v=0f113ab492fa791a4cf2
