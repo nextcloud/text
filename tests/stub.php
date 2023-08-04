4667d49b7619250ea6b926e12f8b29f5d7d43f01
Julius Härtl <jus@bitgrid.net>
2023-08-04 21:48:24 +0200

    chore(psalm): Add assistant event to stub
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- tests/stub.php
+++ tests/stub.php
@@ -3,29 +3,49 @@
 declare(strict_types=1);
 
 namespace OC\Hooks {
-    class Emitter {}
+	class Emitter {
+	}
 }
 
 namespace OC\AppFramework\OCS {
-	class BaseResponse {}
+	class BaseResponse {
+	}
+}
+
+namespace OCA\Files\Event {
+	class LoadAdditionalScriptsEvent extends \OCP\EventDispatcher\Event {
+	}
 }
 
 namespace OCA\Viewer\Event {
-	class LoadViewer extends \OCP\EventDispatcher\Event {}
+	class LoadViewer extends \OCP\EventDispatcher\Event {
+	}
 }
 
 namespace OCA\Files_Sharing\Event {
-	class BeforeTemplateRenderedEvent extends \OCP\EventDispatcher\Event {
+	abstract class BeforeTemplateRenderedEvent extends \OCP\EventDispatcher\Event {
 		abstract public function getShare(): \OCP\Share\IShare;
 	}
 }
 
 namespace OC\User {
-	class NoUserException extends \Exception {}
+	class NoUserException extends \Exception {
+	}
 }
 
 namespace OCA\Files_Sharing {
 	abstract class SharedStorage implements \OCP\Files\Storage\IStorage {
 		abstract public function getShare(): \OCP\Share\IShare;
+	}
+}
+
+namespace OCA\TPAssistant\Event {
+
+	use OCP\TextProcessing\Task;
+
+	abstract class BeforeAssistantNotificationEvent extends \OCP\EventDispatcher\Event {
+		abstract public function getTask(): Task;
+		abstract public function setWantsNotification(bool $wantsNotification): void;
+		abstract public function setNotificationTarget(?string $notificationTarget): void;
 	}
 }
