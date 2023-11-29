9e51d536af861a6ecf5c851c6f0079c3317e6cb5
Jonas <jonas@freesources.org>
2023-11-29 15:47:18 +0100

    test(cypress): Add test to visually compare print view
    Compare print view of long document visually using
    `cypress-visual-regression` to detect regressions in print view in the
    future.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,22 @@
 const { defineConfig } = require('cypress')
+const getCompareSnapshotsPlugin = require('cypress-visual-regression/dist/plugin.js')
 
 module.exports = defineConfig({
 	projectId: 'hx9gqy',
 	viewportWidth: 1280,
 	viewportHeight: 900,
+	screenshotsFolder: './cypress/snapshots/actual',
+	trashAssetsBeforeRuns: true,
+	env: {
+		failSilently: false,
+		type: 'actual',
+		SNAPSHOT_BASE_DIRECTORY: './cypress/snapshots/base',
+		SNAPSHOT_DIFF_DIRECTORY: './cypress/snapshots/diff',
+	},
 	e2e: {
 		setupNodeEvents(on, config) {
+			getCompareSnapshotsPlugin(on, config)
+
 			const browserify = require('@cypress/browserify-preprocessor')
 			const webpack = require('@cypress/webpack-preprocessor')
 			const webpackOptions = require('@nextcloud/webpack-vue-config')
@@ -25,5 +36,5 @@ module.exports = defineConfig({
 		// do not retry in `cypress open`
 		openMode: 0,
 	},
-	"numTestsKeptInMemory": 5,
+	'numTestsKeptInMemory': 5,
 })
