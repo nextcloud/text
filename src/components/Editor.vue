c574149eb2ad0f18a72c8f89288f97543c4d75ba
Max <max@nextcloud.com>
2023-12-11 14:51:49 +0100

    fix(sync): keep queue around during reconnects
    When yjs does not receive awareness updates
    it will close and reopen the websocket.
    Keep the content of the queue, i.e. the outgoing steps
    so they can be send out once the connection is back.
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -329,6 +329,7 @@ export default {
 	},
 	created() {
 		this.$ydoc = new Doc()
+		this.$queue = []
 		// The following can be useful for debugging ydoc updates
 		// this.$ydoc.on('update', function(update, origin, doc, tr) {
 		//   console.debug('ydoc update', update, origin, doc, tr)
@@ -381,6 +382,7 @@ export default {
 				ydoc: this.$ydoc,
 				syncService: this.$syncService,
 				fileId: this.fileId,
+				queue: this.$queue,
 				initialSession: this.initialSession,
 			})
 			this.$providers.push(syncServiceProvider)
