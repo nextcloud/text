d14558b8615b1723bb7fa0ab0bab16aa646b46f6
Jonas <jonas@freesources.org>
2023-12-24 00:29:09 +0100

    feat(paragraph): Don't show link previews
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/nodes/Paragraph.js
+++ src/nodes/Paragraph.js
@@ -1,11 +1,13 @@
 import TiptapParagraph from '@tiptap/extension-paragraph'
-import { VueNodeViewRenderer } from '@tiptap/vue-2'
-import ParagraphView from './ParagraphView.vue'
 
 const Paragraph = TiptapParagraph.extend({
 
-	addNodeView() {
-		return VueNodeViewRenderer(ParagraphView)
+	addOptions() {
+		return {
+			HTMLAttributes: {
+				class: 'paragraph-content',
+			},
+		}
 	},
 
 	parseHTML() {
