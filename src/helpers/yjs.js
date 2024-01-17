456f218414f8b67e6ef542947293c0d8e51dc7a5
Max <max@nextcloud.com>
2024-01-17 20:28:29 +0100

    fix(sync): push local changes on reconnect
    When an editing session is interrupted
    steps that were already pushed to the server
    may be cleared alongside the sync service session.
    
    Make sure to push all local state
    that is not part of the document state
    when (re-)connecting.
    
    Tests
    
    Yjs relies on a browser environment.
    Therefore we test it in a cypress test.
    Move these tests into component tests
    to separate them from 'normal' e2e tests.
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/helpers/yjs.js
+++ src/helpers/yjs.js
@@ -24,21 +24,71 @@ import { encodeArrayBuffer, decodeArrayBuffer } from '../helpers/base64.js'
 import { Doc, encodeStateAsUpdate, applyUpdate } from 'yjs'
 
 /**
+ * Get Document state encode as base64.
  *
- * @param {Doc} ydoc - encode state of this doc
+ * Used to store yjs state on the server.
+ * @param {Y.Doc} ydoc - encode state of this doc
  */
 export function getDocumentState(ydoc) {
-	const update = encodeStateAsUpdate(ydoc)
+	const update = Y.encodeStateAsUpdate(ydoc)
 	return encodeArrayBuffer(update)
 }
 
 /**
  *
- * @param {Doc} ydoc - apply state to this doc
+ * @param {Y.Doc} ydoc - apply state to this doc
  * @param {string} documentState - base64 encoded doc state
  * @param {object} origin - initiator object e.g. WebsocketProvider
  */
 export function applyDocumentState(ydoc, documentState, origin) {
 	const update = decodeArrayBuffer(documentState)
-	applyUpdate(ydoc, update, origin)
+	Y.applyUpdate(ydoc, update, origin)
+}
+
+/**
+ * Update message for everything in ydoc that is not in encodedBaseUpdate
+ *
+ * @param {Y.Doc} ydoc - encode state of this doc
+ * @param {string} encodedBaseUpdate - base64 encoded doc update to build upon
+ */
+export function getUpdateMessage(ydoc, encodedBaseUpdate) {
+	const baseUpdate = decodeArrayBuffer(encodedBaseUpdate)
+	const baseStateVector = Y.encodeStateVectorFromUpdate(baseUpdate)
+	const docStateVector = Y.encodeStateVector(ydoc)
+	if (sameState(baseStateVector, docStateVector)) {
+		// no additional state in the ydoc - early return
+		return
+	}
+	const encoder = encoding.createEncoder()
+	encoding.writeVarUint(encoder, messageSync)
+	const update = Y.encodeStateAsUpdate(ydoc, baseStateVector)
+	syncProtocol.writeUpdate(encoder, update)
+	const buf = encoding.toUint8Array(encoder)
+	return encodeArrayBuffer(buf)
+}
+
+/**
+ * Apply an updated message to the ydoc.
+ *
+ * Only used in tests right now.
+ * @param {Y.Doc} ydoc - encode state of this doc
+ * @param {string} updateMessage - base64 encoded y-websocket sync message with update
+ * @param {object} origin - initiator object e.g. WebsocketProvider
+ */
+export function applyUpdateMessage(ydoc, updateMessage, origin = 'origin') {
+	const updateBuffer = decodeArrayBuffer(updateMessage)
+	const decoder = decoding.createDecoder(updateBuffer)
+	const messageType = decoding.readVarUint(decoder)
+	if (messageType !== messageSync) {
+		console.error('y.js update message with invalid type', messageType)
+		return
+	}
+	// There are no responses to updates - so this is a dummy.
+	const encoder = encoding.createEncoder()
+	syncProtocol.readSyncMessage(
+		decoder,
+		encoder,
+		ydoc,
+		origin,
+	)
 }
