9d3139038cb39633f96a543d41de6809ea9bbd3a
Grigorii K. Shartsev <me@shgk.me>
2024-02-01 11:52:04 +0100

    fix(menubar): name ActionFormattingHelp as NcActionButton to support NcActions
    Signed-off-by: Grigorii K. Shartsev <me@shgk.me>

--- src/components/Menu/ActionFormattingHelp.vue
+++ src/components/Menu/ActionFormattingHelp.vue
@@ -20,25 +20,30 @@
   -
   -->
 <template>
-	<NcActionButton close-after-click
+	<NextcloudVueNcActionButton close-after-click
 		data-text-action-entry="formatting-help"
 		v-on="$listeners">
 		<template #icon>
 			<Help />
 		</template>
 		{{ t('text', 'Formatting help') }}
-	</NcActionButton>
+	</NextcloudVueNcActionButton>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
-import { NcActionButton } from '@nextcloud/vue'
+import { NcActionButton as NextcloudVueNcActionButton } from '@nextcloud/vue'
 import { Help } from '../icons.js'
 
 export default defineComponent({
-	name: 'ActionFormattingHelp',
+	// This component is used as a direct child of NcActions.
+	// Even if it actually renders NcActionButton, NcActions cannot see it due to rendering limitations in Vue.
+	// Though it works in general, NcActions doesn't handle it correctly. See NcActions docs for details.
+	// Hotfix - rename the component to NcActionButton because it represents and renders it.
+	// eslint-disable-next-line vue/match-component-file-name
+	name: 'NcActionButton',
 	components: {
-		NcActionButton,
+		NextcloudVueNcActionButton,
 		Help,
 	},
 })
