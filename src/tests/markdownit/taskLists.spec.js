15c630ae9abd97ac082a10de846e3333ca13ae94
Max <max@nextcloud.com>
2024-02-04 10:37:02 +0100

    test(markdownit): separate directory for markdowit tests
    Split the one markdownit spec into separate ones.
    
    The commonmark tests are also only testing our markdownit.
    Move them into the same folder
    
    Signed-off-by: Max <max@nextcloud.com>

---
+++ src/tests/markdownit/taskLists.spec.js
@@ -0,0 +1,19 @@
+import markdownit from '../../markdownit'
+import stripIndent from './stripIndent'
+
+describe('task list extension', () => {
+
+	it('renders task lists', () => {
+		const rendered = markdownit.render('* [ ] task\n* not a task')
+		expect(stripIndent(rendered)).toBe(stripIndent(`
+			<ul class="contains-task-list" data-bullet="*">
+			<li class="task-list-item "><input class="task-list-item-checkbox" type="checkbox" disabled="" id="task-item-0" />task</li>
+			</ul>
+			<ul data-bullet="*">
+			<li>not a task</li>
+			</ul>`
+			))
+	})
+
+})
+
