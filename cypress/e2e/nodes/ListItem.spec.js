fe74b7bc66ad2bc11fcfd47adc6ab41c0e80246c
Max <max@nextcloud.com>
2024-02-20 20:26:44 +0100

    test(cy): make node test helpers available
    Signed-off-by: Max <max@nextcloud.com>

--- cypress/e2e/nodes/ListItem.spec.js
+++ cypress/e2e/nodes/ListItem.spec.js
@@ -5,11 +5,10 @@ import ListItem from '@tiptap/extension-list-item'
 import TaskList from './../../../src/nodes/TaskList.js'
 import TaskItem from './../../../src/nodes/TaskItem.js'
 import BulletList from './../../../src/nodes/BulletList.js'
-import Markdown, { createMarkdownSerializer } from './../../../src/extensions/Markdown.js'
-import { findChildren } from './../../../src/helpers/prosemirrorUtils.js'
+import Markdown from './../../../src/extensions/Markdown.js'
 import { createCustomEditor } from './../../support/components.js'
 import testData from '../../fixtures/ListItem.md'
-import markdownit from './../../../src/markdownit/index.js'
+import { loadMarkdown, runCommands, expectMarkdown } from './helpers.js'
 
 describe('ListItem extension integrated in the editor', () => {
 
@@ -37,42 +36,9 @@ describe('ListItem extension integrated in the editor', () => {
 			expect(input).to.be.ok
 			expect(output).to.be.ok
 			/* eslint-enable no-unused-expressions */
-			loadMarkdown(input)
-			runCommands()
-			expectMarkdown(output.replace(/\n*$/, ''))
+			loadMarkdown(editor, input)
+			runCommands(editor)
+			expectMarkdown(editor, output.replace(/\n*$/, ''))
 		})
-	}
-
-	const loadMarkdown = (markdown) => {
-		const stripped = markdown.replace(/\t*/g, '')
-		editor.commands.setContent(markdownit.render(stripped))
-	}
-
-	const runCommands = () => {
-		let found
-		while ((found = findCommand())) {
-			const { node, pos } = found
-			const name = node.text
-			editor.commands.setTextSelection(pos)
-			editor.commands[name]()
-			editor.commands.insertContent('did ')
-		}
-	}
-
-	const findCommand = () => {
-		const doc = editor.state.doc
-		return findChildren(doc, child => {
-			return child.isText && Object.prototype.hasOwnProperty.call(editor.commands, child.text)
-		})[0]
-	}
-
-	const expectMarkdown = (markdown) => {
-		const stripped = markdown.replace(/\t*/g, '')
-		expect(getMarkdown()).to.equal(stripped)
-	}
-
-	const getMarkdown = () => {
-		const serializer = createMarkdownSerializer(editor.schema)
-		return serializer.serialize(editor.state.doc)
 	}
 })
---
+++ cypress/e2e/nodes/helpers.js
@@ -0,0 +1,58 @@
+/**
+ * @copyright Copyright (c) 2024 Max <max@nextcloud.com>
+ *
+ * @author Max <max@nextcloud.com>
+ *
+ * @license AGPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import markdownit from './../../../src/markdownit/index.js'
+import { findChildren } from './../../../src/helpers/prosemirrorUtils.js'
+import { createMarkdownSerializer } from './../../../src/extensions/Markdown.js'
+
+export function loadMarkdown(editor, markdown) {
+	const stripped = markdown.replace(/\t*/g, '')
+	editor.commands.setContent(markdownit.render(stripped))
+}
+
+export function runCommands(editor) {
+	let found
+	while ((found = findCommand(editor))) {
+		const { node, pos } = found
+		const name = node.text
+		editor.commands.setTextSelection(pos)
+		editor.commands[name]()
+		editor.commands.insertContent('did ')
+	}
+}
+
+function findCommand(editor){
+	const doc = editor.state.doc
+	return findChildren(doc, child => {
+		return child.isText && Object.prototype.hasOwnProperty.call(editor.commands, child.text)
+	})[0]
+}
+
+export function expectMarkdown(editor, markdown){
+	const stripped = markdown.replace(/\t*/g, '')
+	expect(getMarkdown(editor)).to.equal(stripped)
+}
+
+function getMarkdown(editor) {
+	const serializer = createMarkdownSerializer(editor.schema)
+	return serializer.serialize(editor.state.doc)
+}
