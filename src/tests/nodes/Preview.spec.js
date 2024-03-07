18681f3cae26c7124cef08a4c3974a30bb729f0c
Max <max@nextcloud.com>
2024-03-07 12:04:07 +0100

    fix(Preview): allow removing Preview with backspace
    * Also only allow link marks inside the Preview.
    * See #5442 for list of other follow up fixes for Previews
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/tests/nodes/Preview.spec.js
+++ src/tests/nodes/Preview.spec.js
@@ -1,5 +1,6 @@
 import Preview from './../../nodes/Preview'
 import Markdown from './../../extensions/Markdown'
+import Link from './../../marks/Link'
 import { getExtensionField } from '@tiptap/core'
 import { createCustomEditor, markdownThroughEditor, markdownThroughEditorHtml } from '../helpers'
 import markdownit from '../../markdownit/index.js'
@@ -11,9 +12,7 @@ describe('Preview extension', () => {
 	})
 
 	it('exposes the toMarkdown function in the prosemirror schema', () => {
-		const editor = createCustomEditor({
-			extensions: [Markdown, Preview]
-		})
+		const editor = createEditorWithPreview()
 		const preview = editor.schema.nodes.preview
 		expect(preview.spec.toMarkdown).toBeDefined()
 	})
@@ -32,9 +31,7 @@ describe('Preview extension', () => {
 
 	it('detects links', () => {
 		const link = `<a href="https://nextcloud.com" title="preview">link</a>`
-		const editor = createCustomEditor({
-			extensions: [Markdown, Preview]
-		})
+		const editor = createEditorWithPreview()
 		editor.commands.setContent(`${link}<p>hello></p>`)
 		const node = editor.state.doc.content.firstChild
 		expect(node.type.name).toBe('preview')
@@ -43,3 +40,9 @@ describe('Preview extension', () => {
 	})
 
 })
+
+function createEditorWithPreview() {
+		return createCustomEditor({
+			extensions: [Markdown, Preview, Link]
+		})
+}
