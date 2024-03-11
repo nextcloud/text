f7d50b89088a8ea1ff9da0651c671433940803a9
Max <max@nextcloud.com>
2024-03-11 15:30:21 +0100

    fix(Tables): require at least one cell in the header
    Fixes #4793.
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/nodes/Table/TableHeadRow.js
+++ src/nodes/Table/TableHeadRow.js
@@ -2,7 +2,7 @@ import TableRow from './TableRow.js'
 
 export default TableRow.extend({
 	name: 'tableHeadRow',
-	content: 'tableHeader*',
+	content: 'tableHeader+',
 	allowGapCursor: false,
 
 	toMarkdown(state, node) {
