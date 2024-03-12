a43d77c36b12da1aea7f12a8ff9c8c0348b80527
Jonas <jonas@freesources.org>
2024-03-12 18:14:07 +0100

    fix(backend): Accept pushs with only step1 messages by read-only clients
    This fixes read-only clients getting 403 responses on push requests that
    only contain questions for updates.
    
    Fixes: #5223
    Fixes: #5366
    Fixes: #5368
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- lib/Service/ApiService.php
+++ lib/Service/ApiService.php
@@ -184,11 +184,8 @@ class ApiService {
 
 	/**
 	 * @throws NotFoundException
-	 * @throws DoesNotExistException
-	 *
-	 * @param null|string $token
 	 */
-	public function push(Session $session, Document $document, int $version, array $steps, string $awareness, string|null $token = null): DataResponse {
+	public function push(Session $session, Document $document, int $version, array $steps, string $awareness, ?string $token = null): DataResponse {
 		try {
 			$session = $this->sessionService->updateSessionAwareness($session, $awareness);
 		} catch (DoesNotExistException $e) {
@@ -198,16 +195,12 @@ class ApiService {
 		if (empty($steps)) {
 			return new DataResponse([]);
 		}
-		$file = $this->documentService->getFileForSession($session, $token);
-		if ($this->documentService->isReadOnly($file, $token)) {
-			return new DataResponse([], 403);
-		}
 		try {
-			$result = $this->documentService->addStep($document, $session, $steps, $version);
+			$result = $this->documentService->addStep($document, $session, $steps, $version, $token);
 		} catch (InvalidArgumentException $e) {
 			return new DataResponse($e->getMessage(), 422);
-		} catch (DoesNotExistException $e) {
-			// Session was removed in the meantime. #3875
+		} catch (DoesNotExistException|NotPermittedException) {
+			// Either no write access or session was removed in the meantime (#3875).
 			return new DataResponse([], 403);
 		}
 		return new DataResponse($result);
