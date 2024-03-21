2f19e4104d51704f17823747dda1d1dfaebe2f90
Max <max@nextcloud.com>
2024-03-21 15:52:49 +0100

    fix(links): set preview if whitespace after link selected
    Signed-off-by: Max <max@nextcloud.com>

--- src/nodes/Preview.js
+++ src/nodes/Preview.js
@@ -148,7 +148,7 @@ function previewPossible({ selection }) {
 	if (hasOtherContent($from.parent)) {
 		return false
 	}
-	const href = extractHref($from.nodeAfter || $from.nodeBefore)
+	const href = extractHref($from.parent.firstChild)
 	if (!href || href.startsWith('#')) {
 		return false
 	}
