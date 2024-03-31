89ebc5a4c32ed9bf10ed16b10fbb008eefd6055a
Julius Härtl <jus@bitgrid.net>
2024-03-31 18:30:49 +0200

    ci: Make cypress test more stable by closing connections
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -393,6 +393,36 @@ Cypress.Commands.add('closeFile', (params = {}) => {
 	cy.wait('@close', { timeout: 7000 })
 })
 
+let closeData = null
+Cypress.Commands.add('interceptCreate', () => {
+	return cy.intercept({ method: 'PUT', url: '**/session/*/create' }, (req) => {
+		closeData = {
+			url: ('' + req.url).replace('create', 'close'),
+		}
+		req.continue((res) => {
+			closeData = {
+				...closeData,
+				...res.body,
+			}
+		})
+	}).as('create')
+})
+
+Cypress.Commands.add('closeInterceptedSession', (shareToken = undefined) => {
+	return cy.window().then(win => {
+		return axios.post(
+			closeData.url,
+			{
+				documentId: closeData.session.documentId,
+				sessionId: closeData.session.id,
+				sessionToken: closeData.session.token,
+				token: shareToken,
+			},
+			{ headers: { requesttoken: win.OC.requestToken } },
+		)
+	})
+})
+
 Cypress.Commands.add('getFile', fileName => {
 	return cy.get(`[data-cy-files-list] tr[data-cy-files-list-row-name="${fileName}"]`)
 
