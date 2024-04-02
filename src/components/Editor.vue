499a54f9585c9935117bb511ae3abc6477bc05bd
Julius Härtl <jus@bitgrid.net>
2024-04-02 12:48:25 +0200

    fix: Adapt to review feedback
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -495,7 +495,7 @@ export default {
 					this.$queue.push(updateMessage)
 				}
 			} else {
-				this.setInitialYjsState(documentSource, { isRich: this.isRichEditor })
+				this.setInitialYjsState(documentSource, { isRichEditor: this.isRichEditor })
 			}
 
 			this.hasConnectionIssue = false
