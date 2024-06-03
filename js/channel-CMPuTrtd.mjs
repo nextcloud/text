0b25d94c8af69d5405d1361f9011e392d1be9a6e
nextcloud-command <nextcloud-command@users.noreply.github.com>
2024-06-03 19:45:58 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/channel-CMPuTrtd.mjs
@@ -0,0 +1,2 @@
+/*! third party licenses: js/vendor.LICENSE.txt */
+import{c6 as c,c7 as o}from"./RichText-BZ2es64B.mjs";const a=(s,t)=>c.lang.round(o.parse(s)[t]);export{a as c};
---
+++ js/clone-B2Jvvztg.mjs
@@ -0,0 +1,2 @@
+/*! third party licenses: js/vendor.LICENSE.txt */
+import{c as r}from"./graph-CmUrnE8h.mjs";var n=4;function a(t){return r(t,n)}export{a as c};
