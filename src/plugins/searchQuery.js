2a30da6d7746474552e59b644aee330105d056a3
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-07-17 09:23:07 -0400

    fix: Begin adding ability to specify match index to highlight
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- src/plugins/searchQuery.js
+++ src/plugins/searchQuery.js
@@ -18,13 +18,17 @@ export function searchQuery() {
 	return new Plugin({
 		key: searchQueryPluginKey,
 		state: {
-			init: () => ({ query: '' }),
+			init: () => ({ query: '', all: true, index: 0 }),
 			apply: (tr, oldState) => {
 				const searchQuery = tr.getMeta('searchQuery')
+				const all = tr.getMeta('all')
+				const index = tr.getMeta('index')
 
 				if (searchQuery !== undefined) {
 					return {
 						query: searchQuery,
+						all,
+						index,
 					}
 				} else {
 					return oldState
@@ -34,6 +38,9 @@ export function searchQuery() {
 	})
 }
 
-export const setSearchQuery = (query) => ({ tr }) => {
-	return tr.setMeta('searchQuery', query)
+export const setSearchQuery = (query, all, index) => ({ tr }) => {
+	return tr
+		.setMeta('searchQuery', query)
+		.setMeta('all', all)
+		.setMeta('index', index)
 }
