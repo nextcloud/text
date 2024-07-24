3c6a02499dc2f90aacabea1182fe5dd2ed143bb3
Max <max@nextcloud.com>
2024-07-24 13:03:24 +0200

    fix(listeners): clean up sessions for plaintext as well
    Signed-off-by: Max <max@nextcloud.com>

--- lib/Listeners/BeforeNodeWrittenListener.php
+++ lib/Listeners/BeforeNodeWrittenListener.php
@@ -31,7 +31,6 @@ use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
 use OCP\Files\Events\Node\BeforeNodeWrittenEvent;
 use OCP\Files\File;
-use OCP\Files\NotFoundException;
 
 /**
  * @template-implements IEventListener<Event|BeforeNodeWrittenEvent>
@@ -48,19 +47,17 @@ class BeforeNodeWrittenListener implements IEventListener {
 			return;
 		}
 		$node = $event->getNode();
+		if (!$node instanceof File) {
+			return;
+		}
+		if ($this->documentService->isSaveFromText()) {
+			return;
+		}
+		// Reset document session to avoid manual conflict resolution if there's no unsaved steps
 		try {
-			if ($node instanceof File && $node->getMimeType() === 'text/markdown') {
-				if (!$this->documentService->isSaveFromText()) {
-					// Reset document session to avoid manual conflict resolution if there's no unsaved steps
-					try {
-						$this->documentService->resetDocument($node->getId());
-					} catch (DocumentHasUnsavedChangesException) {
-						// Do not throw during event handling in this is expected to happen
-					}
-				}
-			}
-		} catch (NotFoundException) {
-			// This might occur on new files when a NonExistingFile is passed and we cannot access the mimetype
+			$this->documentService->resetDocument($node->getId());
+		} catch (DocumentHasUnsavedChangesException) {
+			// Do not throw during event handling in this is expected to happen
 		}
 	}
 }
