713707c828f89910ea877472fb32eaddc98e3174
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-08-12 11:56:13 -0400

    feat(test): load fixture into editor
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- cypress/component/editor/search.cy.js
+++ cypress/component/editor/search.cy.js
@@ -1,25 +1,28 @@
-import { createCustomEditor } from '../../support/components.js'
+import { Editor } from '@tiptap/core'
+import { Document } from '@tiptap/extension-document'
+import { Text } from '@tiptap/extension-text'
 import Search from '../../../src/extensions/Search.js'
+import Paragraph from '../../../src/nodes/Paragraph.js'
+import HardBreak from '../../../src/nodes/HardBreak.js'
 
 describe('editor search highlighting', () => {
-	const editor = createCustomEditor({
-		content: 'lorem ipsum',
-		extensions: [Search],
-	})
+	let editor = null
 
 	before(() => {
-		editor.setOptions({
-			element: document.querySelector('div[data-cy-root]')
+		cy.fixture('lorem.txt').then((text) => {
+			editor = new Editor({
+				element: document.querySelector('div[data-cy-root]'),
+				content: text,
+				extensions: [Document, Text, Search, Paragraph, HardBreak],
+			})
 		})
 	})
 
-	beforeEach(() => editor.createView())
-
 	it('can highlight a search', () => {
-		editor.commands.setSearchQuery('lorem')
+		const searchQuery = 'Lorem ipsum dolor sit amet'
+		editor.commands.setSearchQuery(searchQuery)
 
-		const highlightElements = document.querySelector('span[data-text-el]')
-
-		expect(highlightElements)
+		const highlightedElement = document.querySelector('span[data-text-el]')
+		expect(highlightedElement.innerText).to.equal(searchQuery)
 	})
 })
