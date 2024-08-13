4f8d0751d3b4c6a55a930f9eddca256281a6ace2
Julius Härtl <jus@bitgrid.net>
2024-08-13 22:31:05 +0200

    test(cypress): Wait for contenteditable before typing
    Signed-off-by: Julius Härtl <jus@bitgrid.net>

--- cypress/e2e/conflict.spec.js
+++ cypress/e2e/conflict.spec.js
@@ -116,6 +116,7 @@ function createConflict(fileName, mime) {
 	cy.visit('/apps/files')
 	cy.openFile(fileName)
 	cy.log('Inspect editor')
+	cy.getEditor().find('.ProseMirror').should('have.attr', 'contenteditable', 'true')
 	cy.getContent()
 		.type('Hello you cruel conflicting world')
 	cy.uploadFile(fileName, mime)
