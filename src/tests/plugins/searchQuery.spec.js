3a4d61112bdd9f33aaa6f46e69b8059b130b67f0
Elizabeth Danzberger <lizzy7128@tutanota.de>
2024-08-13 10:34:21 -0400

    feat: Move search highlighting to editor API (#6199)
    * feat(search): Move to editor API instead of event bus
    
    Signed-off-by: Elizabeth Danzberger <lizzy7128@tutanota.de>

--- src/tests/plugins/searchQuery.spec.js
+++ src/tests/plugins/searchQuery.spec.js
@@ -5,7 +5,7 @@
 import { searchQuery } from '../../plugins/searchQuery.js'
 import { Plugin, EditorState } from '@tiptap/pm/state'
 import { schema } from '@tiptap/pm/schema-basic'
-import { setSearchQuery, nextMatch } from '../../plugins/searchQuery.js'
+import { setSearchQuery, nextMatch, previousMatch } from '../../plugins/searchQuery.js'
 
 describe('searchQuery plugin', () => {
 	it('can set up plugin and state', () => {
@@ -59,6 +59,29 @@ describe('searchQuery plugin', () => {
 			query: 'lorem',   // search query should be the same
 			matchAll: false,  // matchAll is set to false
 			index: 1,         // index is incremented to the next match
+		})
+	})
+
+	it ('can accept previous match state', () => {
+		const { plugin, state } = pluginSetup()
+
+		const setSearch = setSearchQuery('lorem')(state)
+		const previousSearch = previousMatch()(state)
+
+		let newState = state.apply(setSearch)
+
+		expect(plugin.getState(newState)).toEqual({
+			query: 'lorem',
+			matchAll: true,
+			index: 0,
+		})
+		
+		newState = newState.apply(previousSearch)
+
+		expect(plugin.getState(newState)).toEqual({
+			query: 'lorem',
+			matchAll: false,
+			index: -1,
 		})
 	})
 })
