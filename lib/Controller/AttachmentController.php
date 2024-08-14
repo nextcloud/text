3e75d26a0e0dd655f3144d03ccf9e6f80ebd2475
Jonas <jonas@freesources.org>
2024-08-14 16:50:16 +0200

    fix(attachments): Uploading of attachments from public shares
    The session middleware expects the share token as param `token`.
    
    Fixes: #6206
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- lib/Controller/AttachmentController.php
+++ lib/Controller/AttachmentController.php
@@ -126,7 +126,7 @@ class AttachmentController extends ApiController implements ISessionAwareControl
 	#[NoAdminRequired]
 	#[PublicPage]
 	#[RequireDocumentSession]
-	public function uploadAttachment(?string $shareToken = null): DataResponse {
+	public function uploadAttachment(?string $token = null): DataResponse {
 		$documentId = $this->getSession()->getDocumentId();
 
 		try {
@@ -137,8 +137,8 @@ class AttachmentController extends ApiController implements ISessionAwareControl
 					throw new Exception('Could not read file');
 				}
 				$newFileName = $file['name'];
-				if ($shareToken) {
-					$uploadResult = $this->attachmentService->uploadAttachmentPublic($documentId, $newFileName, $newFileResource, $shareToken);
+				if ($token) {
+					$uploadResult = $this->attachmentService->uploadAttachmentPublic($documentId, $newFileName, $newFileResource, $token);
 				} else {
 					$userId = $this->getSession()->getUserId();
 					$uploadResult = $this->attachmentService->uploadAttachment($documentId, $newFileName, $newFileResource, $userId);
