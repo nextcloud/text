6dc2fed1670a8a4efea9746b694f40e8aab200f2
nextcloud-command <nextcloud-command@users.noreply.github.com>
2024-08-24 08:16:30 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/clone-Bn0cqcga.chunk.mjs
+++
@@ -1,2 +0,0 @@
-/*! third party licenses: js/vendor.LICENSE.txt */
-import{a as r}from"./graph-w_G7dLWf.chunk.mjs";var n=4;function e(t){return r(t,n)}export{e as c};
--- js/logger-BjUg4RMP.chunk.mjs
+++
@@ -1,2 +0,0 @@
-/*! third party licenses: js/vendor.LICENSE.txt */
-import{g as t}from"./index-DedwEVqf.chunk.mjs";const o=t().setApp("text").detectUser().build();export{o as l};
