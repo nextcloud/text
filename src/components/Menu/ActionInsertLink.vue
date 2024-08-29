e25c9aef3534dc3cab28dfe254071f06b132dd3d
Luka Trovic <luka@nextcloud.com>
2024-08-29 17:08:55 +0200

    fix: hide menu Link to file for direct editing
    Signed-off-by: Luka Trovic <luka@nextcloud.com>

--- src/components/Menu/ActionInsertLink.vue
+++ src/components/Menu/ActionInsertLink.vue
@@ -1,22 +1,22 @@
 <!--
-  - @copyright Copyright (c) 2022
-  -
-  - @license AGPL-3.0-or-later
-  -
-  - This program is free software: you can redistribute it and/or modify
-  - it under the terms of the GNU Affero General Public License as
-  - published by the Free Software Foundation, either version 3 of the
-  - License, or (at your option) any later version.
-  -
-  - This program is distributed in the hope that it will be useful,
-  - but WITHOUT ANY WARRANTY; without even the implied warranty of
-  - MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  - GNU Affero General Public License for more details.
-  -
-  - You should have received a copy of the GNU Affero General Public License
-  - along with this program. If not, see <http://www.gnu.org/licenses/>.
-  -
-  -->
+	- @copyright Copyright (c) 2022
+	-
+	- @license AGPL-3.0-or-later
+	-
+	- This program is free software: you can redistribute it and/or modify
+	- it under the terms of the GNU Affero General Public License as
+	- published by the Free Software Foundation, either version 3 of the
+	- License, or (at your option) any later version.
+	-
+	- This program is distributed in the hope that it will be useful,
+	- but WITHOUT ANY WARRANTY; without even the implied warranty of
+	- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	- GNU Affero General Public License for more details.
+	-
+	- You should have received a copy of the GNU Affero General Public License
+	- along with this program. If not, see <http://www.gnu.org/licenses/>.
+	-
+	-->
 <template>
 	<NcActions ref="menu"
 		class="entry-action entry-action__insert-link"
@@ -41,8 +41,10 @@
 			</template>
 			{{ t('text', 'Remove link') }}
 		</NcActionButton>
-		<NcActionButton close-after-click
+		<NcActionButton v-if="!isUsingDirectEditing"
+			ref="buttonFile"
 			:data-text-action-entry="`${actionEntry.key}-file`"
+			close-after-click
 			@click="linkFile">
 			<template #icon>
 				<Document />
@@ -81,6 +83,7 @@
 import { NcActions, NcActionButton, NcActionInput } from '@nextcloud/vue'
 import { getLinkWithPicker } from '@nextcloud/vue/dist/Components/NcRichText.js'
 import { FilePicker, FilePickerType } from '@nextcloud/dialogs'
+import { loadState } from '@nextcloud/initial-state'
 
 import { getMarkAttributes, isActive } from '@tiptap/core'
 
@@ -112,6 +115,7 @@ export default {
 			href: null,
 			isInputMode: false,
 			startPath: null,
+			isUsingDirectEditing: loadState('text', 'directEditingToken', null) !== null,
 		}
 	},
 	computed: {
