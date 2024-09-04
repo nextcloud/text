12e5fb26ce1b0a8d3a36ff54246ff533d3872e79
Jonas <jonas@freesources.org>
2024-09-04 18:09:49 +0200

    fix(mention): No extra whitespace in markdown before/after mentions
    Fixes: #6314
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/extensions/Mention.js
+++ src/extensions/Mention.js
@@ -35,8 +35,6 @@ export default TipTapMention.extend({
 	},
 
 	toMarkdown(state, node) {
-		state.write(' ')
 		state.write(`@[${node.attrs.label}](mention://user/${encodeURIComponent(node.attrs.id)})`)
-		state.write(' ')
 	},
 })
