c23ece2d7b9036ea62b89ad7dde0cd2817c4612a
Jonas <jonas@freesources.org>
2024-09-17 23:56:08 +0200

    fix(paragraph): get rid of superfluous ParagraphView component
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/nodes/Paragraph.js
+++ src/nodes/Paragraph.js
@@ -1,13 +1,8 @@
 import TiptapParagraph from '@tiptap/extension-paragraph'
 import { VueNodeViewRenderer } from '@tiptap/vue-2'
 import previewOptions from '../plugins/previewOptions.js'
-import ParagraphView from './ParagraphView.vue'
 
 const Paragraph = TiptapParagraph.extend({
-
-	addNodeView() {
-		return VueNodeViewRenderer(ParagraphView)
-	},
 
 	parseHTML() {
 		return this.parent().map(rule => Object.assign(rule, { preserveWhitespace: 'full' }))
