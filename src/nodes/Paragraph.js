2cb1c5457a052856463d52045f444702181ba2d2
Jonas <jonas@freesources.org>
2024-09-17 18:29:11 +0200

    feat(previewOptions): Migrate preview options to prosemirror decorations
    Fixes: #6185
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/nodes/Paragraph.js
+++ src/nodes/Paragraph.js
@@ -5,6 +5,7 @@
 
 import TiptapParagraph from '@tiptap/extension-paragraph'
 import { VueNodeViewRenderer } from '@tiptap/vue-2'
+import previewOptions from '../plugins/previewOptions.js'
 import ParagraphView from './ParagraphView.vue'
 
 const Paragraph = TiptapParagraph.extend({
@@ -39,6 +40,14 @@ const Paragraph = TiptapParagraph.extend({
 				return false
 			},
 		}
+	},
+
+	addProseMirrorPlugins() {
+		return [
+			previewOptions({
+				editor: this.editor,
+			}),
+		]
 	},
 })
 
