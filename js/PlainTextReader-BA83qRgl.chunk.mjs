620c039eea9a7413d4421b3f9fa806d61dc0ec8b
nextcloud-command <nextcloud-command@users.noreply.github.com>
2024-10-01 08:38:19 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/PlainTextReader-BA83qRgl.chunk.mjs
@@ -0,0 +1,2 @@
+/*! third party licenses: js/vendor.LICENSE.txt */
+import{T as r,au as n}from"./RichText-BbI1dRRP.chunk.mjs";import{n as o,e as s}from"./_plugin-vue2_normalizer-CBD_Jxxq.chunk.mjs";import{B as a}from"./BaseReader-Cj9MYAos.chunk.mjs";const p={name:"PlainTextReader",components:{BaseReader:a},provide:{renderHtml(e){return"<pre>"+s(e)+"</pre>"},extensions:()=>[r,n]},props:{content:{type:String,required:!0}}};var i=function(){var e=this,t=e._self._c;return t("BaseReader",{attrs:{content:e.content}})},c=[],m=o(p,i,c,!1,null,null);const f=m.exports;export{f as P};
--- js/channel-nbdM4U3D.chunk.mjs
+++
@@ -1,2 +0,0 @@
-/*! third party licenses: js/vendor.LICENSE.txt */
-import{al as t,am as o}from"./mermaid.core-DY0PheJ_.chunk.mjs";const n=(a,s)=>t.lang.round(o.parse(a)[s]);export{n as c};
