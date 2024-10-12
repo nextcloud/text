fc13808a2112e7e164e3ad4c980f30559d2f6e81
Max <max@nextcloud.com>
2024-10-12 16:24:14 +0200

    enh(types): add global and vue shims
    Signed-off-by: Max <max@nextcloud.com>

---
+++ src/global.d.ts
@@ -0,0 +1,17 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+/// <reference types="@nextcloud/typings" />
+
+declare global {
+	interface Window {
+		OCA: Record<string, any>
+		OCP: Nextcloud.v29.OCP
+	}
+
+	const appVersion: string
+}
+
+export {}
---
+++ src/vue-shim.d.ts
@@ -0,0 +1,9 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+declare module "*.vue" {
+	import Vue from 'vue'
+	export default Vue
+}
