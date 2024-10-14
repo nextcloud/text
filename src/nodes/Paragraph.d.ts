bdd20d61321f5d89aac68c878e5e5885aba7ace5
Max <max@nextcloud.com>
2024-10-14 07:35:52 +0200

    fix(types): add type declaration for Paragraph node
    `createCustomEditor.ts` test script imports `Paragraph`
    and `tsc` complains without type definitions.
    
    Signed-off-by: Max <max@nextcloud.com>

---
+++ src/nodes/Paragraph.d.ts
@@ -0,0 +1,9 @@
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type { Node } from '@tiptap/core'
+
+declare const Paragraph: Node
+export default Paragraph
