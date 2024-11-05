1821ce853a54971243fcccaa87687c009a8ee2e6
Jonas <jonas@freesources.org>
2024-11-05 10:33:45 +0100

    test(cypress): More specific selector for viewer header in links test
    Fixes failing cypress test "link bubble -> closes the link bubble when
    clicking elsewhere".
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- cypress/e2e/nodes/Links.spec.js
+++ cypress/e2e/nodes/Links.spec.js
@@ -67,7 +67,9 @@ describe('test link marks', function() {
 				.find('.widget-default--name')
 				.contains('Nextcloud')
 
-			cy.get('[role="dialog"] h2.modal-header__name').click()
+			cy.get('[role="dialog"] h2.modal-header__name')
+				.contains(fileName)
+				.click()
 
 			cy.get('.link-view-bubble .widget-default')
 				.should('not.exist')
