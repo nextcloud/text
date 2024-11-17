0b83dbbbe3db8512081e6d86d95696d7cfcfe81e
Max <max@nextcloud.com>
2024-11-17 20:52:59 +0100

    refactor(sync): rename _receiveSteps to receiveSteps
    It has always been publicly called from the PollingBackend.
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/services/SyncService.js
+++ src/services/SyncService.js
@@ -196,7 +196,7 @@ class SyncService {
 				this.pushError = 0
 				this.sending = false
 				if (steps?.length > 0) {
-					this._receiveSteps({ steps })
+					this.receiveSteps({ steps })
 				}
 			}).catch(err => {
 				const { response, code } = err
@@ -227,7 +227,7 @@ class SyncService {
 			})
 	}
 
-	_receiveSteps({ steps, document = null, sessions = [] }) {
+	receiveSteps({ steps, document = null, sessions = [] }) {
 		const awareness = sessions
 			.filter(s => s.lastContact > (Math.floor(Date.now() / 1000) - COLLABORATOR_DISCONNECT_TIME))
 			.filter(s => s.lastAwarenessMessage)
