49a9705ed1c58db3a39b71ef264756d2733f6bf4
Max <max@nextcloud.com>
2024-11-22 19:55:49 +0100

    refactor(sync): cache steps in sync service
    Store the steps that need to be send
    where we also do the debouncing.
    
    They will be updated whenever there is a new message from y-websocket.
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -347,7 +347,6 @@ export default {
 	},
 	created() {
 		this.$ydoc = new Doc()
-		this.$queue = []
 		// The following can be useful for debugging ydoc updates
 		// this.$ydoc.on('update', function(update, origin, doc, tr) {
 		//   console.debug('ydoc update', update, origin, doc, tr)
@@ -398,7 +397,6 @@ export default {
 				ydoc: this.$ydoc,
 				syncService: this.$syncService,
 				fileId: this.fileId,
-				queue: this.$queue,
 				initialSession: this.initialSession,
 				disableBC: true,
 			})
@@ -698,8 +696,10 @@ export default {
 		},
 
 		async close() {
-			await this.$syncService.sendRemainingSteps(this.$queue)
+			await this.$syncService.sendRemainingSteps()
+				.catch(err => logger.warn('Failed to send remaining steps', { err }))
 			await this.disconnect()
+				.catch(err => logger.warn('Failed to disconnect', { err }))
 			if (this.$editor) {
 				try {
 					this.unlistenEditorEvents()
