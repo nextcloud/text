858b5219692d3ac30b820e992f661d0f29578342
Max <max@nextcloud.com>
2025-03-04 22:42:50 +0100

    chore(refactor): SyncStatus component
    Signed-off-by: Max <max@nextcloud.com>

--- src/components/Editor/DocumentStatus/SyncStatus.vue
+++ src/components/Editor/DocumentStatus/SyncStatus.vue
@@ -1,27 +1,13 @@
 <!--
-  - SPDX-FileCopyrightText: 2022 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-FileCopyrightText: 2022-2025 Nextcloud GmbH and Nextcloud contributors
   - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 
 <template>
-	<NcNoteCard v-if="hasWarning" type="warning">
-		<p v-if="isLoadingError">
-			{{ syncError.data.data.error }}
-			<!-- Display reload button on PRECONDITION_FAILED response type -->
-			<a v-if="syncError.data.status === 412" class="button primary" @click="reload">{{ t('text', 'Reload') }}</a>
-		</p>
-		<p v-else-if="hasSyncCollission">
-			{{ t('text', 'Document has been changed outside of the editor. The changes cannot be applied') }}
-		</p>
-		<p v-else-if="hasConnectionIssue">
-			{{ t('text', 'Document could not be loaded. Please check your internet connection.') }}
-			<a class="button primary" @click="reconnect">{{ t('text', 'Reconnect') }}</a>
-		</p>
-	</NcNoteCard>
-	<NcNoteCard v-else-if="idle" type="info">
-		<p>
-			{{ t('text', 'Document idle for {timeout} minutes, click to continue editing', { timeout: IDLE_TIMEOUT }) }}
-			<a class="button primary" @click="reconnect">{{ t('text', 'Reconnect') }}</a>
+	<NcNoteCard v-if="type" :type="type">
+		<p v-if="message">
+			{{ message }}
+			<a v-if="button" class="button primary" @click="button.action">{{ button.text }}</a>
 		</p>
 	</NcNoteCard>
 </template>
@@ -30,6 +16,12 @@
 
 import { ERROR_TYPE, IDLE_TIMEOUT } from '../../../services/SyncService.js'
 import { NcNoteCard } from '@nextcloud/vue'
+
+const MESSAGES = {
+	changedOutside: t('text', 'Document has been changed outside of the editor. The changes cannot be applied'),
+	couldNotBeLoaded: t('text', 'Document could not be loaded. Please check your internet connection.'),
+	idle: t('text', 'Document idle for {timeout} minutes, click to continue editing', { timeout: IDLE_TIMEOUT }),
+}
 
 export default {
 	name: 'SyncStatus',
@@ -60,11 +52,38 @@ export default {
 	},
 
 	computed: {
+		type() {
+			return ( this.hasWarning && 'warning' ) || ( this.idle && 'info' )
+		},
+		message() {
+			return (this.isLoadingError && this.syncError.data.data.error)
+				|| (this.hasSyncCollission && MESSAGES.changedOutside)
+				|| (this.hasConnectionIssue && MESSAGES.couldNotBeLoaded)
+				|| (this.idle && MESSAGES.idle)
+		},
+		button() {
+			// Display reload button on PRECONDITION_FAILED response
+			if (this.isPreconditionFailed) {
+				return {
+					text: t('text', 'Reload'),
+					action: this.reload
+				}
+			}
+			if (this.hasConnectionIssue || this.idle ) {
+				return {
+					text: t('text', 'Reconnect'),
+					action: this.reconnect
+				}
+			}
+		},
 		hasSyncCollission() {
 			return this.syncError && this.syncError.type === ERROR_TYPE.SAVE_COLLISSION
 		},
 		isLoadingError() {
 			return this.syncError && this.syncError.type === ERROR_TYPE.LOAD_ERROR
+		},
+		isPreconditionFailed() {
+			return this.isLoadingError && this.syncError.data.status === 412
 		},
 		hasWarning() {
 			return this.syncError || this.hasConnectionIssue
