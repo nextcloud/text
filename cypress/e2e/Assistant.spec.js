88e115d69c6746a8086e9629745aa4681d779e67
Julius Knorr <jus@bitgrid.net>
2025-03-17 22:03:45 +0100

    tests(cypress): Wait for input to be focused
    Signed-off-by: Julius Knorr <jus@bitgrid.net>

--- cypress/e2e/Assistant.spec.js
+++ cypress/e2e/Assistant.spec.js
@@ -101,13 +101,18 @@ describe('Assistant', () => {
 			.should('be.visible')
 			.click()
 
+		cy.wait(1000)
+
 		cy.get('[data-cy="translate-input"]')
 			.should('be.visible')
-			.click()
+			.focus()
+
+		cy.wait(1000)
+
+		cy.get('[data-cy="translate-input"]')
+			.should('be.focused')
 		cy.get('[data-cy="translate-input"]')
 			.should('be.visible')
 			.type('Hello World')
-		cy.get('[data-cy="translate-input"]')
-			.should('be.focused')
 	})
 })
