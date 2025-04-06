f937d34d49d6502a774c1063d22f140853ec4bac
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-04-06 03:37:51 +0000

    fix(deps): Fix npm audit
    Signed-off-by: GitHub <noreply@github.com>

--- js/chunk-4BMEZGHF-BRW7jBLk.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{_ as i}from"./mermaid.core-LfoD7IgF.chunk.mjs";function e(t,c){t.accDescr&&c.setAccDescription?.(t.accDescr),t.accTitle&&c.setAccTitle?.(t.accTitle),t.title&&c.setDiagramTitle?.(t.title)}i(e,"populateCommonDb");export{e as p};
-//# sourceMappingURL=chunk-4BMEZGHF-BRW7jBLk.chunk.mjs.map
