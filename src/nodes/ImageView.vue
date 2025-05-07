f4bf663180d76612b446fcaee6b4f41b6ac67676
Jonas <jonas@freesources.org>
2025-05-07 17:26:46 +0200

    fix(image): Only focus image description on newly inserted image
    Fixes: #6843
    Fixes: nextcloud/collectives#1740
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/nodes/ImageView.vue
+++ src/nodes/ImageView.vue
@@ -170,6 +170,7 @@ export default {
 			showImageModal: false,
 			imageIndex: null,
 			isEditable: false,
+			isLastInserted: false,
 			embeddedImageList: [],
 		}
 	},
@@ -223,6 +224,12 @@ export default {
 		this.editor.on('update', ({ editor }) => {
 			this.isEditable = editor.isEditable
 		})
+		this.editor.on('transaction', ({ transaction }) => {
+			const trMeta = transaction.getMeta('insertedAttachmentSrc')
+			if (trMeta?.src === this.src) {
+				this.isLastInserted = true
+			}
+		})
 		this.loadPreview().catch(this.onImageLoadFailure)
 	},
 	methods: {
@@ -265,7 +272,9 @@ export default {
 		onLoaded() {
 			this.loaded = true
 			this.$nextTick(() => {
-				this.$refs.altInput?.focus()
+				if (this.isLastInserted) {
+					this.$refs.altInput?.focus()
+				}
 			})
 		},
 		async updateEmbeddedImageList() {
