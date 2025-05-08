1ccb176971e64ba254e1156773d4cd0900653764
Jonas <jonas@freesources.org>
2025-05-08 09:48:33 +0200

    chore(keymap): Remove obsolete <Mod>-<F> overwrite
    The keymap extension was not in use for quite some time and I'm now
    longer able to reproduce issues with <Mod>-<F> without the overwrite
    either.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/extensions/Keymap.js
+++ src/extensions/Keymap.js
@@ -7,7 +7,7 @@ import { Extension } from '@tiptap/core'
 import { Plugin } from '@tiptap/pm/state'
 
 const Keymap = Extension.create({
-	name: 'customkeymap',
+	name: 'Keymap',
 
 	addKeyboardShortcuts() {
 		return this.options
@@ -19,19 +19,16 @@ const Keymap = Extension.create({
 				props: {
 					handleKeyDown(view, event) {
 						const key = event.key || event.keyCode
+
+						/**
+						 * <Mod>-<Del>
+						 * Overwrite Viewer keybinding to delete the file
+						 */
 						if (
 							(event.ctrlKey || event.metaKey)
 							&& !event.shiftKey
-							&& (key === 'f' || key === 70)
+							&& event.key === 'Delete'
 						) {
-							// We need to stop propagation and dispatch the event on the window
-							// in order to force triggering the browser native search in the text editor
-							event.stopPropagation()
-							window.dispatchEvent(event)
-							return true
-						}
-						if (event.key === 'Delete' && event.ctrlKey === true) {
-							// Prevent deleting the file, by core Viewer.vue
 							event.stopPropagation()
 							window.dispatchEvent(event)
 							return true
