2837b261e42a2de0fe02a5994d1c67ec3df88216
Max <max@nextcloud.com>
2025-06-19 09:46:03 +0200

    test(nodes): destroy editor after use
    Fixes errors during test runs such as https://github.com/nextcloud/text/actions/runs/15742170977/job/44369984600
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/tests/nodes/Preview.spec.js
+++ src/tests/nodes/Preview.spec.js
@@ -3,6 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import { test as baseTest } from 'vitest'
 import Preview from './../../nodes/Preview.js'
 import Markdown from './../../extensions/Markdown.js'
 import Link from './../../marks/Link.js'
@@ -13,33 +14,39 @@ import {
 } from '../testHelpers/markdown.js'
 import createCustomEditor from '../testHelpers/createCustomEditor.ts'
 
+const test = baseTest.extend({
+	editor: async ({ task: _ }, use) => {
+		const editor = createCustomEditor('', [Markdown, Preview, Link])
+		await use(editor)
+		editor.destroy()
+	}
+})
+
 describe('Preview extension', () => {
-	it('exposes toMarkdown function', () => {
+	test('exposes toMarkdown function', () => {
 		const toMarkdown = getExtensionField(Preview, 'toMarkdown', Preview)
 		expect(typeof toMarkdown).toEqual('function')
 	})
 
-	it('exposes the toMarkdown function in the prosemirror schema', () => {
-		const editor = createEditorWithPreview()
+	test('exposes the toMarkdown function in the prosemirror schema', ({ editor }) => {
 		const preview = editor.schema.nodes.preview
 		expect(preview.spec.toMarkdown).toBeDefined()
 	})
 
-	it('markdown syntax is preserved through editor', () => {
+	test('markdown syntax is preserved through editor', () => {
 		const markdown = '[link](https://nextcloud.com (preview))'
 		expect(markdownThroughEditor(markdown)).toBe(markdown)
 	})
 
-	it('serializes HTML to markdown', () => {
+	test('serializes HTML to markdown', () => {
 		const markdown = '[link](https://nextcloud.com (preview))'
 		const link = '<a href="https://nextcloud.com" title="preview">link</a>'
 		expect(markdownThroughEditorHtml(link))
 			.toBe(markdown)
 	})
 
-	it('detects links', () => {
+	test('detects links', ({ editor }) => {
 		const link = '<a href="https://nextcloud.com" title="preview">link</a>'
-		const editor = createEditorWithPreview()
 		editor.commands.setContent(`${link}<p>hello></p>`)
 		const node = editor.state.doc.content.firstChild
 		expect(node.type.name).toBe('preview')
@@ -49,4 +56,3 @@ describe('Preview extension', () => {
 
 })
 
-const createEditorWithPreview = () => createCustomEditor('', [Markdown, Preview, Link])
