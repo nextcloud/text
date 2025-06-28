fabc79eed511d3e69b7681b4e81849b9815f0bd8
Max <max@nextcloud.com>
2025-06-28 20:36:30 +0200

    test(RichTextReader): update content
    Use fixtures to initialize wrapper and destroy after the test
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/tests/components/RichTextReader.spec.js
+++ src/tests/components/RichTextReader.spec.js
@@ -4,17 +4,28 @@
  */
 
 import { mount } from '@vue/test-utils'
-import { test, expect } from 'vitest'
+import { test as baseTest, expect } from 'vitest'
 import RichTextReader from '../../components/RichTextReader.vue'
 import { nextTick } from 'vue'
 
-const content = '# Hello world\n\n[this is a link](https://nextcloud.com)'
-test('renders markdown', async () => {
-	const wrapper = mount(RichTextReader, {
-		propsData: { content },
-	})
-	await nextTick()
+const test = baseTest.extend({
+	content: ({ task: _ }, use) => use(''),
+	wrapper: async ({ content }, use) => {
+		const wrapper = mount(RichTextReader, { propsData: { content } })
+		await nextTick()
+		await use(wrapper)
+		wrapper.destroy()
+	}
+})
+
+test.scoped({ content: '# Hello world\n\n[this is a link](https://nextcloud.com)' })
+test('renders markdown', async ({ wrapper }) => {
 	expect(wrapper.get('h1').text()).toBe('#Hello world') // # is the heading anchor
 	expect(wrapper.get('a[href="https://nextcloud.com"]').text()).toBe('this is a link')
-	wrapper.destroy()
+})
+
+test('updates markdown', async ({ wrapper }) => {
+	await wrapper.setProps({ content: '## Hello world' })
+	expect(wrapper.get('h2').text()).toBe('#Hello world') // # is the heading anchor
+	expect(wrapper.find('a[href="https://nextcloud.com"]').exists()).toBeFalsy()
 })
