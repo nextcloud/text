32eff67228bda1946c4912fffae81b8aa3a7ee8a
Max <max@nextcloud.com>
2025-07-20 14:21:48 +0200

    chore(cleanup): unused updateSession and dependencies
    Signed-off-by: Max <max@nextcloud.com>

--- src/services/SessionConnection.js
+++ src/services/SessionConnection.js
@@ -2,17 +2,6 @@
  * SPDX-FileCopyrightText: 2022 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-import axios from '@nextcloud/axios'
-import { generateUrl } from '@nextcloud/router'
-
-export class ConnectionClosedError extends Error {
-	constructor(
-		message = 'Close has already been called on the connection',
-		...rest
-	) {
-		super(message, ...rest)
-	}
-}
 
 export class SessionConnection {
 	#content
@@ -67,25 +56,6 @@ export class SessionConnection {
 		return this.#hasOwner
 	}
 
-	get #defaultParams() {
-		return {
-			documentId: this.#document.id,
-			sessionId: this.#session.id,
-			sessionToken: this.#session.token,
-			token: this.connection.shareToken,
-		}
-	}
-
-	// TODO: maybe return a new connection here so connections have immutable state
-	update(guestName) {
-		return this.#post(this.#url(`session/${this.#document.id}/session`), {
-			...this.#defaultParams,
-			guestName,
-		}).then(({ data }) => {
-			this.#session = data
-		})
-	}
-
 	close() {
 		this.closed = true
 	}
@@ -94,29 +64,4 @@ export class SessionConnection {
 	setBaseVersionEtag(baseVersionEtag) {
 		this.#document.baseVersionEtag = baseVersionEtag
 	}
-
-	#post(...args) {
-		if (this.closed) {
-			return Promise.reject(new ConnectionClosedError())
-		}
-		return axios.post(...args)
-	}
-
-	#url(endpoint) {
-		const isPublic = !!this.#defaultParams.token
-		return _endpointUrl(endpoint, isPublic)
-	}
-}
-
-/**
- *
- * @param {string} endpoint - endpoint of the url inside apps/text
- * @param {boolean} isPublic - public url or not
- */
-function _endpointUrl(endpoint, isPublic = false) {
-	const _baseUrl = generateUrl('/apps/text')
-	if (isPublic) {
-		return `${_baseUrl}/public/${endpoint}`
-	}
-	return `${_baseUrl}/${endpoint}`
 }
