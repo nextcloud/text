80a773c0c57975ef131d518ffd624130043df7b0
Jonas <jonas@freesources.org>
2025-07-23 16:19:41 +0200

    fix(LinkBubble): Read `editor.contentComponent` when it's ready
    When initiating the editor extensions (i.e. in `addProseMirrorPlugins()`),
    `editor.contentComponent` is not yet available. So let's read it when we
    actually use it, when creating the tooltip in `LinkBubblePluginView`.
    
    Before, a new Vue instance was initiated for each `LinkBubbleView.vue`
    as the `parent` given to `VueRenderer()` was `undefined`. With this fix,
    `LinkBubbleView.vue` becomes a child component of the root editor Vue
    instance.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/plugins/LinkBubblePluginView.js
+++ src/plugins/LinkBubblePluginView.js
@@ -58,7 +58,7 @@ class LinkBubblePluginView {
 		}
 
 		this.#component ||= new VueRenderer(LinkBubbleView, {
-			parent: this.options.parent,
+			parent: this.options.editor.contentComponent,
 			propsData: {
 				editor: this.options.editor,
 				href: null,
