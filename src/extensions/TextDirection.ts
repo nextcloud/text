7dfc6d2e1ce091203d5e5a6ba37d5234722369c9
Jonas <jonas@freesources.org>
2025-07-28 19:18:33 +0200

    fix(TextDirection): Ignore unchanged nodes in appendTransaction
    Amongh other things, fixes the `undoInputRule()` command.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/extensions/TextDirection.ts
+++ src/extensions/TextDirection.ts
@@ -61,12 +61,13 @@ function TextDirectionPlugin({ types }: { types: string[] }) {
 					if (node.attrs.dir !== null && node.textContent.length > 0) {
 						return
 					}
+					const newTextDirection = getTextDirection(node.textContent)
+					if (node.attrs.dir === newTextDirection) {
+						return
+					}
+
 					const marks = tr.storedMarks || []
-					tr.setNodeAttribute(
-						pos,
-						'dir',
-						getTextDirection(node.textContent),
-					)
+					tr.setNodeAttribute(pos, 'dir', newTextDirection)
 					// `tr.setNodeAttribute` resets the stored marks so we'll restore them
 					for (const mark of marks) {
 						tr.addStoredMark(mark)
