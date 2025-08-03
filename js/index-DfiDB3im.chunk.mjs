788eba5895f6438b36917e5ae4fa953b721bfcbc
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-08-03 01:43:41 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/index-DfiDB3im.chunk.mjs
+++
@@ -1,2 +0,0 @@
-function e(t,r,n){const o=document.querySelector(`#initial-state-${t}-${r}`);if(o===null){if(n!==void 0)return n;throw new Error(`Could not find initial state ${r} of ${t}`)}try{return JSON.parse(atob(o.value))}catch{throw new Error(`Could not parse initial state ${r} of ${t}`)}}export{e as l};
-//# sourceMappingURL=index-DfiDB3im.chunk.mjs.map
--- js/text-public.mjs
+++ js/text-public.mjs
@@ -1,2 +1,2 @@
-import{l as e}from"./index-DfiDB3im.chunk.mjs";OCA.Text={RichWorkspaceEnabled:e("text","workspace_available"),OpenReadOnlyEnabled:e("text","open_read_only_enabled")};
+import{l as e}from"./index-B8nYQ_zQ.chunk.mjs";OCA.Text={RichWorkspaceEnabled:e("text","workspace_available"),OpenReadOnlyEnabled:e("text","open_read_only_enabled")};
 //# sourceMappingURL=text-public.mjs.map
