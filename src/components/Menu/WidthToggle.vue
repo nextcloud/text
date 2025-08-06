55d4c65855089ef476976987ccaf4b4d76419fd5
Max <max@nextcloud.com>
2025-08-06 10:23:03 +0200

    chore(cleanup): suprefluous redrawEditor after width toggle
    Signed-off-by: Max <max@nextcloud.com>

--- src/components/Menu/WidthToggle.vue
+++ src/components/Menu/WidthToggle.vue
@@ -12,19 +12,7 @@
 <script setup>
 import { t } from '@nextcloud/l10n'
 import NcActionCheckbox from '@nextcloud/vue/components/NcActionCheckbox'
-import { nextTick, watch } from 'vue'
-import { useEditor } from '../../composables/useEditor.ts'
 import { useEditorWidth } from '../../composables/useEditorWidth.ts'
 
-const { editor } = useEditor()
 const { isFullWidth, setFullWidth } = useEditorWidth()
-
-const redrawEditor = () => {
-	nextTick(() => {
-		const { commands, view } = editor
-		view.updateState(view.state)
-		commands.focus()
-	})
-}
-watch(isFullWidth, redrawEditor)
 </script>
