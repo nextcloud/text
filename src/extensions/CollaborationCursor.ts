f5794cb0a4b20a5bb3a1cf409c35fc826ce35080
Max <max@nextcloud.com>
2025-08-21 11:43:40 +0200

    fix(selection): use clientId arg in render function
    The clientId arg is not exposed in the tiptap type signature
    but provided by y-prosemirror anyway.
    
    See https://github.com/yjs/y-prosemirror/blob/f89fadd2a8bf15c0c7e6bfcac268883a5fa4e0f8/src/plugins/cursor-plugin.js#L118
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/extensions/CollaborationCursor.ts
+++ src/extensions/CollaborationCursor.ts
@@ -5,6 +5,11 @@
 
 import { CollaborationCursor as TiptapCollaborationCursor } from '@tiptap/extension-collaboration-cursor'
 
+interface AwarenessUser {
+	color: string
+	name: string
+}
+
 /**
  * @param clientId The Yjs client ID
  */
@@ -31,26 +36,30 @@ function getTimestamp() {
 	return Math.floor(Date.now() / 1000)
 }
 
+/**
+ * Render the cursor decoration
+ *
+ * @param user the users awareness data
+ * @param clientId not part of the tiptap type signature but provided by y-prosemirror
+ */
+function render(user: AwarenessUser, clientId?: number): HTMLElement {
+	const cursor = document.createElement('span')
+	cursor.classList.add('collaboration-cursor__caret')
+	cursor.setAttribute('style', `border-color: ${user.color}`)
+	const label = document.createElement('div')
+	label.classList.add('collaboration-cursor__label')
+	label.id = `collaboration-cursor__label__${clientId}`
+	label.setAttribute('style', `background-color: ${user.color}`)
+	label.insertBefore(document.createTextNode(user.name), null)
+	cursor.insertBefore(label, null)
+	return cursor
+}
+
 const CollaborationCursor = TiptapCollaborationCursor.extend({
 	addOptions() {
 		return {
 			...this.parent?.(),
-			render: (user) => {
-				const cursor = document.createElement('span')
-
-				cursor.classList.add('collaboration-cursor__caret')
-				cursor.setAttribute('style', `border-color: ${user.color}`)
-
-				const label = document.createElement('div')
-
-				label.classList.add('collaboration-cursor__label')
-				label.id = `collaboration-cursor__label__${user.clientId}`
-				label.setAttribute('style', `background-color: ${user.color}`)
-				label.insertBefore(document.createTextNode(user.name), null)
-				cursor.insertBefore(label, null)
-
-				return cursor
-			},
+			render,
 		}
 	},
 
