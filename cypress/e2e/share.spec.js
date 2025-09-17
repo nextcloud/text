5515deb4b3d2cb3d605d28b9b7b27119af7aa500
Jonas <jonas@freesources.org>
2025-09-17 10:09:55 +0200

    text(cy): Fix CSS selectors for readonly bar
    Signed-off-by: Jonas <jonas@freesources.org>

--- cypress/e2e/share.spec.js
+++ cypress/e2e/share.spec.js
@@ -41,7 +41,7 @@ describe('Open test.md in viewer', function () {
 					.find('h2')
 					.should('contain', 'Hello world')
 
-				cy.get('.text-editor--readonly-bar')
+				cy.get('[data-text-el="readonly-bar"]')
 					.getActionEntry('outline')
 					.click()
 
