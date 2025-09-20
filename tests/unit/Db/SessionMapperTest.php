608e8978f2d1a3c6a3e7053da27a30a05496f124
Benjamin Frueh <benjamin.frueh@gmail.com>
2025-09-20 23:23:15 +0200

    Add test for deleteOrphanedSteps method
    Signed-off-by: Benjamin Frueh <benjamin.frueh@gmail.com>

--- tests/unit/Db/SessionMapperTest.php
+++ tests/unit/Db/SessionMapperTest.php
@@ -135,4 +135,61 @@ class SessionMapperTest extends \Test\TestCase {
 		self::assertCount(1, $remainingSessions);
 		self::assertEquals($recentSession->getId(), $remainingSessions[0]->getId());
 	}
+
+	public function testDeleteOrphanedSteps() {
+		$this->stepMapper->deleteAll(1);
+		$this->sessionMapper->deleteByDocumentId(1);
+
+		// Create session
+		$session = $this->sessionMapper->insert(Session::fromParams([
+			'userId' => 'admin',
+			'documentId' => 1,
+			'lastContact' => time(),
+			'token' => uniqid(),
+			'color' => '00ff00',
+		]));
+
+		// Create steps for session
+		$this->stepMapper->insert(Step::fromParams([
+			'sessionId' => $session->getId(),
+			'documentId' => 1,
+			'data' => 'YJSDATA1',
+			'version' => 1,
+		]));
+		$this->stepMapper->insert(Step::fromParams([
+			'sessionId' => $session->getId(),
+			'documentId' => 1,
+			'data' => 'YJSDATA2',
+			'version' => 2,
+		]));
+
+		// Create orphaned steps
+		$this->stepMapper->insert(Step::fromParams([
+			'sessionId' => 99999, // Non-existent session
+			'documentId' => 1,
+			'data' => 'ORPHANED1',
+			'version' => 3,
+		]));
+		$this->stepMapper->insert(Step::fromParams([
+			'sessionId' => 99998, // Non-existent session
+			'documentId' => 1,
+			'data' => 'ORPHANED2',
+			'version' => 4,
+		]));
+
+		// Verify 4 steps total
+		$allSteps = $this->stepMapper->find(1, 0);
+		self::assertCount(4, $allSteps);
+
+		// Delete orphaned steps
+		$deletedCount = $this->sessionMapper->deleteOrphanedSteps();
+
+		// Should have deleted 2 orphaned steps
+		self::assertEquals(2, $deletedCount);
+
+		// Should have 2 valid steps remaining
+		$remainingSteps = $this->stepMapper->find(1, 0);
+		self::assertCount(2, $remainingSteps);
+>>>>>>> a66cd75d2 (Add test for deleteOrphanedSteps method)
+	}
 }
