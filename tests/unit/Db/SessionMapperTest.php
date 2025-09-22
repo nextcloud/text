9c0aea5d67ffefa966b78a6f10105d74417383bc
Benjamin Frueh <benjamin.frueh@gmail.com>
2025-09-22 17:24:58 +0200

    Only delete sessions with only steps below lastSavedVersion
    Signed-off-by: Benjamin Frueh <benjamin.frueh@gmail.com>

--- tests/unit/Db/SessionMapperTest.php
+++ tests/unit/Db/SessionMapperTest.php
@@ -9,12 +9,13 @@ class SessionMapperTest extends \Test\TestCase {
 
 	private SessionMapper $sessionMapper;
 	private StepMapper $stepMapper;
+	private DocumentMapper $documentMapper;
 
 	public function setUp(): void {
 		parent::setUp();
 		$this->sessionMapper = \OCP\Server::get(SessionMapper::class);
 		$this->stepMapper = \OCP\Server::get(StepMapper::class);
-
+		$this->documentMapper = \OCP\Server::get(DocumentMapper::class);
 	}
 
 	public function testDeleteInactiveWithoutSteps() {
@@ -100,23 +101,40 @@ class SessionMapperTest extends \Test\TestCase {
 	}
 
 	public function testDeleteOldSessions() {
-		$this->stepMapper->deleteAll(1);
-		$this->sessionMapper->deleteByDocumentId(1);
+		$this->documentMapper->clearAll();
+		$this->sessionMapper->clearAll();
+		$this->stepMapper->clearAll();
 
 		$fourMonthsAgo = time() - (120 * 24 * 60 * 60);
 		$oneWeekAgo = time() - (7 * 24 * 60 * 60);
 
-		// Create old and recent session
+		// Create document
+		$document = $this->documentMapper->insert(Document::fromParams([
+			'id' => 1,
+			'currentVersion' => 0,
+			'lastSavedVersion' => 100,
+			'lastSavedVersionTime' => time()
+		]));
+
+		// Create sessions
 		$oldSession = $this->sessionMapper->insert(Session::fromParams([
+			'id' => 1,
 			'userId' => 'admin',
-			'documentId' => 1,
+			'documentId' => $document->getId(),
+			'lastContact' => $fourMonthsAgo,
+			'token' => uniqid(),
+			'color' => '00ff00',
+		]));
+		$oldSessionWithNewVersion = $this->sessionMapper->insert(Session::fromParams([
+			'userId' => 'admin',
+			'documentId' => $document->getId(),
 			'lastContact' => $fourMonthsAgo,
 			'token' => uniqid(),
 			'color' => '00ff00',
 		]));
 		$recentSession = $this->sessionMapper->insert(Session::fromParams([
 			'userId' => 'admin',
-			'documentId' => 1,
+			'documentId' => $document->getId(),
 			'lastContact' => $oneWeekAgo,
 			'token' => uniqid(),
 			'color' => 'ff0000',
@@ -124,35 +142,46 @@ class SessionMapperTest extends \Test\TestCase {
 
 		// Create steps for old and recent session
 		$this->stepMapper->insert(Step::fromParams([
+			'id' => 1,
 			'sessionId' => $oldSession->getId(),
 			'documentId' => 1,
 			'data' => 'OLD_STEP',
 			'version' => 1,
 		]));
 		$this->stepMapper->insert(Step::fromParams([
+			'id' => 101,
+			'sessionId' => $oldSessionWithNewVersion->getId(),
+			'documentId' => 1,
+			'data' => 'OLD_STEP_NEW_VERSION',
+			'version' => 1,
+		]));
+		$this->stepMapper->insert(Step::fromParams([
+			'id' => 2,
 			'sessionId' => $recentSession->getId(),
 			'documentId' => 1,
 			'data' => 'RECENT_STEP',
 			'version' => 2,
 		]));
 
-		// Verify 2 sessions and 2 steps
-		self::assertCount(2, $this->sessionMapper->findAll(1));
-		self::assertCount(2, $this->stepMapper->find(1, 0));
+		// Verify 3 sessions and 3 steps
+		self::assertCount(3, $this->sessionMapper->findAll(1));
+		self::assertCount(3, $this->stepMapper->find(1, 0));
 
 		// Delete sessions older than 90 days
 		$threeMonths = 90 * 24 * 60 * 60;
 		$deletedCount = $this->sessionMapper->deleteOldSessions($threeMonths);
 		self::assertEquals(1, $deletedCount);
 
-		// Should have 1 recent session remaining
+		// Should have 2 sessions remaining
 		$remainingSessions = $this->sessionMapper->findAll(1);
-		self::assertCount(1, $remainingSessions);
-		self::assertEquals($recentSession->getId(), $remainingSessions[0]->getId());
+		self::assertCount(2, $remainingSessions);
+		self::assertEquals($oldSessionWithNewVersion->getId(), $remainingSessions[0]->getId());
+		self::assertEquals($recentSession->getId(), $remainingSessions[1]->getId());
 
-		// Should have 1 step from recent session remaining
+		// Should have 2 step remaining
 		$remainingSteps = $this->stepMapper->find(1, 0);
-		self::assertCount(1, $remainingSteps);
+		self::assertCount(2, $remainingSteps);
 		self::assertEquals($recentSession->getId(), $remainingSteps[0]->getSessionId());
+		self::assertEquals($oldSessionWithNewVersion->getId(), $remainingSteps[1]->getSessionId());
 	}
 }
