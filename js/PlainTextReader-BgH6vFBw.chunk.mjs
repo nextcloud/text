ac33b1dae620ebcedfec76ae4abc957b0149e050
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-10-06 11:36:19 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/PlainTextReader-BgH6vFBw.chunk.mjs
@@ -0,0 +1,2 @@
+import{P as n}from"./EditorOutline-DBXfGHf1.chunk.mjs";import{B as r}from"./BaseReader-CPcq8YhY.chunk.mjs";import{n as o}from"./_plugin-vue2_normalizer-XSZyZ-Zg.chunk.mjs";const s={name:"PlainTextReader",components:{BaseReader:r},provide:{extensions:()=>[n]},props:{content:{type:String,required:!0}}};var a=function(){var e=this,t=e._self._c;return t("BaseReader",{attrs:{content:e.content}})},i=[],p=o(s,a,i,!1,null,null);const l=p.exports;export{l as P};
+//# sourceMappingURL=PlainTextReader-BgH6vFBw.chunk.mjs.map
