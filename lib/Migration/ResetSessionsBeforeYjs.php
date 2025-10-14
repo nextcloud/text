77996e9eb35128a523a9e85c65987da599970d39
Jonas <jonas@freesources.org>
2025-10-14 11:02:27 +0200

    fix(sync): Reset document sessions on upgrade
    A bug in synchronizing the document state might have resulted in a Yjs
    document state with missing steps being persisted into the Ydoc that we
    store on the server. If no client that had the missing step did an
    autosave afterwards, future sync session members will never recover from
    this situation.
    
    We implemented several improvements recently that should result in less
    out-of-sync situations where a step becomes missing. To start with fresh
    editing sessions and not carry on with possibly unrecoverable document
    states, let's reset all editing sessions with the next app update.
    
    See #7692 for further context.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- lib/Migration/ResetSessionsBeforeYjs.php
+++ lib/Migration/ResetSessionsBeforeYjs.php
@@ -29,7 +29,7 @@ class ResetSessionsBeforeYjs implements IRepairStep {
 	public function run(IOutput $output): void {
 		$appVersion = $this->config->getValueString('text', 'installed_version');
 
-		if (!$appVersion || version_compare($appVersion, '4.0.1') !== -1) {
+		if (!$appVersion || version_compare($appVersion, '5.0.2') !== -1) {
 			return;
 		}
 
