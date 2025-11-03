bc38a040193fcfa02417132f5cc182d82ebb2f0d
Max <max@nextcloud.com>
2025-11-03 20:52:23 +0100

    chore(test): basic editor page object model
    Signed-off-by: Max <max@nextcloud.com>

---
+++ playwright/support/sections/EditorSection.ts
@@ -0,0 +1,25 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type { Locator, Page } from '@playwright/test'
+
+export class EditorSection {
+	public readonly locator: Locator
+	public readonly contentLocator: Locator
+
+	// eslint-disable-next-line no-useless-constructor
+	constructor(public readonly page: Page) {
+		this.locator = this.page.locator('.editor').first()
+		this.contentLocator = this.locator.getByRole('textbox')
+	}
+
+	public async type(keys: string): Promise<void> {
+		await this.contentLocator.pressSequentially(keys)
+	}
+
+	public getHeading(options: {} = {}): Locator {
+		return this.contentLocator.getByRole('heading', options)
+	}
+}
