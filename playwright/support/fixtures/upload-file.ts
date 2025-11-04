dc1bbc0bb5e2aeb5a6586b552d51b4012bc04264
Max <max@nextcloud.com>
2025-11-04 11:14:08 +0100

    chore(test): move File class to separate file
    Signed-off-by: Max <max@nextcloud.com>

--- playwright/support/fixtures/upload-file.ts
+++ playwright/support/fixtures/upload-file.ts
@@ -3,74 +3,12 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { expect, type Page } from '@playwright/test'
 import { test as base } from './request-token'
+import { File } from './File'
 
 interface UploadMdFixture {
 	file: File
 	fileName: string
-}
-
-class File {
-	name: string
-	page: Page
-	requestToken: string
-	id?: number
-
-	constructor(name: string, page: Page, requestToken: string) {
-		this.name = name
-		this.page = page
-		this.requestToken = requestToken
-	}
-
-	async upload(fileContent: string) {
-
-		// Upload file via WebDAV using page.request with requesttoken header
-		const response = await this.page.request.put(
-			`/remote.php/webdav/${this.name}`,
-			{
-				data: fileContent,
-				headers: {
-					'Content-Type': 'text/markdown',
-					'requesttoken': this.requestToken,
-				},
-			},
-		)
-
-		if (!response.ok()) {
-			throw new Error(`Failed to upload file: ${response.status()} ${response.statusText()}`)
-		}
-
-		// Extract file ID from response headers
-		const ocFileId = response.headers()['oc-fileid']
-		const fileId = ocFileId ? Number(ocFileId.split('oc')?.[0]) : 0
-		this.id = fileId
-	}
-
-	async open() {
-		await this.page.goto(`f/${this.id}`)
-		await expect(this.page.getByLabel(this.name, { exact: true }))
-			.toBeVisible()
-	}
-
-	async close() {
-		await this.page.getByRole('button', { name: 'Close', exact: true }).click()
-		await expect(this.page.getByLabel(this.name, { exact: true }))
-			.not.toBeVisible()
-	}
-
-	async move(newName: string) {
-		await this.page.request.fetch(
-			`/remote.php/webdav/${this.name}`,
-			{
-			headers: {
-				Destination: `/remote.php/webdav/${newName}`,
-				'requesttoken': this.requestToken,
-			},
-			method: 'MOVE',
-		})
-		this.name = newName
-	}
 }
 
 /**
