b404b710929f3b4e68c5b4e976a9777c10c9fded
Max <max@nextcloud.com>
2025-11-13 09:52:33 +0100

    chore(test): convert smart picker test to playwright
    Signed-off-by: Max <max@nextcloud.com>

--- playwright/support/sections/EditorSection.ts
+++ playwright/support/sections/EditorSection.ts
@@ -9,21 +9,27 @@ import { expect } from '@playwright/test'
 export class EditorSection {
 	public readonly locator: Locator
 	public readonly content: Locator
-	public readonly sessionList: Locator
-	public readonly offlineState: Locator
-	public readonly saveIndicator: Locator
 	public readonly formattingHelp: Locator
+	public readonly offlineState: Locator
+	public readonly referencePicker: Locator
+	public readonly referenceWidget: Locator
+	public readonly saveIndicator: Locator
+	public readonly sessionList: Locator
+	public readonly suggestions: Locator
 
 	// eslint-disable-next-line no-useless-constructor
 	constructor(public readonly page: Page) {
 		this.locator = this.page.locator('.editor').first()
 		this.content = this.locator.getByRole('textbox')
-		this.sessionList = this.locator.locator('.session-list')
-		this.offlineState = this.locator.locator('.offline-state')
-		this.saveIndicator = this.locator.locator('.save-status')
 		this.formattingHelp = this.page.getByRole('dialog', {
 			name: 'Formatting and shortcuts',
 		})
+		this.offlineState = this.locator.locator('.offline-state')
+		this.referencePicker = this.page.locator('.reference-picker input')
+		this.referenceWidget = this.page.locator('.reference-widget')
+		this.saveIndicator = this.locator.locator('.save-status')
+		this.sessionList = this.locator.locator('.session-list')
+		this.suggestions = this.page.locator('.tippy-box .suggestion-list')
 	}
 
 	public async type(keys: string): Promise<void> {
@@ -55,7 +61,6 @@ export class EditorSection {
 		await this.getMenu(name).click() // close the menu
 	}
 
-	public getHeading(options: object = {}): Locator {
-		return this.content.getByRole('heading', options)
-	}
+	getHeading = (options: object = {}) => this.content.getByRole('heading', options)
+	getSuggestion = (name: string) => this.suggestions.getByText(name)
 }
