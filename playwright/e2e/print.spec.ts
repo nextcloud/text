70e288b1bb68958582cecab3aaeca9e95b5b0a17
Max <max@nextcloud.com>
2025-11-16 09:28:15 +0100

    chore(test): separate screenshots based on host
    Include a host identifier in the test name
    so the screenshot name also includes it.
    
    This way screenshots will only be compared
    to the ones created on the same machine.
    
    Only track the CI ones in git. Ignore the others.
    
    > Having different screenshots between local and docker environments is expected.
    > You can only compare screenshots that are taken in the same exact environment, OS, fonts, etc.
    See https://github.com/microsoft/playwright/issues/20366#issuecomment-1404459692
    
    Signed-off-by: Max <max@nextcloud.com>

--- playwright/e2e/print.spec.ts
+++ playwright/e2e/print.spec.ts
@@ -4,6 +4,7 @@
  */
 
 import { expect, mergeTests } from '@playwright/test'
+import { hostname } from 'os'
 import { test as editorTest } from '../support/fixtures/editor'
 import { loadFixture } from '../support/fixtures/loadFixture'
 import { test as sharedFileTest } from '../support/fixtures/shared-file'
@@ -11,10 +12,11 @@ import { test as uploadFileTest } from '../support/fixtures/upload-file'
 
 const fileTest = mergeTests(editorTest, uploadFileTest)
 const shareTest = mergeTests(editorTest, sharedFileTest)
+const host = process.env.CI ? 'ci' : hostname()
 
 new Map([
-	['Own file', fileTest],
-	['Shared file', shareTest],
+	[`Own file on ${host}`, fileTest],
+	[`Shared file on ${host}`, shareTest],
 ]).forEach((test, name) => {
 	test.use({ fileContent: loadFixture('print.md') })
 	test(name, async ({ open, page }) => {
