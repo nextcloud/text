2609f57167f192ab4d9a711a9249088687b9abec
Max <max@nextcloud.com>
2025-11-18 10:56:30 +0100

    chore(test): versions with ViewerSection
    Signed-off-by: Max <max@nextcloud.com>

---
+++ playwright/support/sections/ViewerSection.ts
@@ -0,0 +1,28 @@
+/**
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import type { Locator, Page } from '@playwright/test'
+
+export class ViewerSection {
+	public readonly el: Locator
+
+	constructor(
+		fileName: string,
+		public readonly page: Page,
+	) {
+		this.el = this.page.getByRole('dialog', { name: fileName })
+	}
+
+	public async openSidebarTab(name: 'Versions') {
+		await this.clickAction('Open sidebar')
+		await this.page.getByRole('tab', { name }).click()
+		return this.page.getByRole('tabpanel', { name })
+	}
+
+	public async clickAction(name: string): Promise<void> {
+		await this.el.getByLabel('Actions', { exact: true }).click()
+		await this.page.getByRole('menuitem', { name }).click()
+	}
+}
