906efdce72394e699cda714a5a5f9f3de33d4e5b
silver <s.szmajduch@posteo.de>
2025-12-11 16:55:14 +0100

    adapt PlainTableContentEditor for stable32
    Use editorFlagsKey with provide() instead of provideEditorFlags()
    composable
    
    Signed-off-by: silver <s.szmajduch@posteo.de>

--- src/components/Editor/PlainTableContentEditor.vue
+++ src/components/Editor/PlainTableContentEditor.vue
@@ -12,30 +12,23 @@
 
 <script>
 import { Editor } from '@tiptap/core'
+/* eslint-disable-next-line import/no-named-as-default */
 import History from '@tiptap/extension-history'
-import MainContainer from './MainContainer.vue'
-import Wrapper from './Wrapper.vue'
-import ContentContainer from './ContentContainer.vue'
+import { provide, watch } from 'vue'
+import { provideEditor } from '../../composables/useEditor.ts'
+import { editorFlagsKey } from '../../composables/useEditorFlags.ts'
+import { useEditorMethods } from '../../composables/useEditorMethods.ts'
 import { PlainTable } from '../../extensions/index.js'
 import { createMarkdownSerializer } from '../../extensions/Markdown.js'
-import { EDITOR, IS_PUBLIC, IS_RICH_EDITOR, IS_RICH_WORKSPACE, EDITOR_UPLOAD } from '../Editor.provider.js'
 import markdownit from '../../markdownit/index.js'
+import { EDITOR_UPLOAD } from '../Editor.provider.ts'
+import ContentContainer from './ContentContainer.vue'
+import MainContainer from './MainContainer.vue'
+import Wrapper from './Wrapper.vue'
 
 export default {
 	name: 'PlainTableContentEditor',
 	components: { ContentContainer, MainContainer, Wrapper },
-
-	provide() {
-		const val = {}
-		Object.defineProperties(val, {
-			[EDITOR]: { get: () => this.$editor },
-			[IS_PUBLIC]: { get: () => false },
-			[IS_RICH_EDITOR]: { get: () => false },
-			[IS_RICH_WORKSPACE]: { get: () => false },
-			[EDITOR_UPLOAD]: { get: () => false },
-		})
-		return val
-	},
 
 	props: {
 		content: {
@@ -50,30 +43,55 @@ export default {
 
 	emits: ['update:content', 'ready', 'create:content'],
 
-	computed: {
-		editor() {
-			return this.$editor
-		},
+	setup(props) {
+		const html = markdownit.render(props.content)
+		const extensions = [PlainTable, History]
+
+		const editor = new Editor({
+			content: html,
+			extensions,
+			editable: !props.readOnly,
+		})
+
+		const { setEditable, setContent } = useEditorMethods(editor)
+
+		watch(
+			() => props.content,
+			(content) => {
+				const html = markdownit.render(content)
+				setContent(html)
+			},
+		)
+
+		watch(
+			() => props.readOnly,
+			(readOnly) => {
+				setEditable(!readOnly)
+			},
+		)
+
+		provideEditor(editor)
+		provide(editorFlagsKey, {
+			isPublic: false,
+			isRichEditor: false,
+			isRichWorkspace: false,
+		})
+		provide(EDITOR_UPLOAD, false)
+
+		return { editor, setContent }
 	},
 
 	created() {
-		const html = markdownit.render(this.content)
-		const extensions = [PlainTable, History]
-
-		this.$editor = new Editor({
-			content: html,
-			extensions,
-			editable: !this.readOnly,
-		})
-
-		this.$editor.on('create', () => {
+		this.editor.on('create', () => {
 			this.$emit('ready')
 			this.$parent.$emit('ready')
 
 			try {
-				const markdown = createMarkdownSerializer(this.$editor.schema).serialize(this.$editor.state.doc)
+				const markdown = createMarkdownSerializer(
+					this.editor.schema,
+				).serialize(this.editor.state.doc)
 				this.emit('create:content', {
-					json: this.$editor.state.doc,
+					json: this.editor.state.doc,
 					markdown,
 				})
 			} catch (error) {
@@ -81,9 +99,11 @@ export default {
 			}
 		})
 
-		this.$editor.on('update', ({ editor }) => {
+		this.editor.on('update', ({ editor }) => {
 			try {
-				const markdown = createMarkdownSerializer(editor.schema).serialize(editor.state.doc)
+				const markdown = createMarkdownSerializer(editor.schema).serialize(
+					editor.state.doc,
+				)
 				this.emit('update:content', {
 					json: editor.state.doc,
 					markdown,
@@ -94,12 +114,8 @@ export default {
 		})
 	},
 
-	updated() {
-		this.$editor.setEditable(!this.readOnly)
-	},
-
 	beforeDestroy() {
-		this.$editor.destroy()
+		this.editor.destroy()
 	},
 
 	methods: {
@@ -116,7 +132,3 @@ export default {
 	},
 }
 </script>
-
-<style scoped>
-
-</style>
