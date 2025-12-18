0f2acfff6b1e1a7b746569da425e9ed95a579164
nextcloud-command <nextcloud-command@users.noreply.github.com>
2025-12-18 01:44:49 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/RichTextReader-DXn-g5gN.chunk.mjs
+++ js/RichTextReader-DXn-g5gN.chunk.mjs
@@ -1,2 +1,2 @@
 import{R as o}from"./EditorOutline-BACHwWz9.chunk.mjs";import{B as i}from"./BaseReader-Cvu1-Yg9.chunk.mjs";import{n as s}from"./_plugin-vue2_normalizer-wd1ZAbRr.chunk.mjs";const c={name:"RichTextReader",components:{BaseReader:i},provide:{extensions(){return[o.configure({editing:!1})]}},props:{content:{type:String,required:!0}}};var a=function(){var e=this,t=e._self._c;return t("BaseReader",{attrs:{content:e.content},on:{"click-link":(n,r)=>e.$emit("click-link",n,r)}})},p=[],m=s(c,a,p,!1,null,null);const u=m.exports;export{u as R};
-//# sourceMappingURL=RichTextReader-cW6xMFWq.chunk.mjs.map
+//# sourceMappingURL=RichTextReader-DXn-g5gN.chunk.mjs.map
