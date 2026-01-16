3d035e50f2bf068bf0e2dbce7a1ae87402b97cb7
Max <max@nextcloud.com>
2026-01-16 09:32:00 +0100

    fix(sync): make bc channel depend on base version etag
    Broadcast is disabled right now.
    Make sure two editing sessions for the same file to not mix when enabling it.
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -300,6 +300,7 @@ export default defineComponent({
 			dirty,
 			editor,
 			el,
+			getBaseVersionEtag,
 			hasConnectionIssue,
 			isPublic,
 			isRichEditor,
@@ -492,6 +493,7 @@ export default defineComponent({
 				initialSession: this.initialSession,
 				disableBc: true,
 				awareness: this.awareness,
+				baseVersionEtag: this.getBaseVersionEtag(),
 			})
 		},
 
