73f96b3e5f4610d72424595699384949f617d3f2
Jonas <jonas@freesources.org>
2026-03-08 23:16:58 +0100

    fix(links): allow inline code within links
    The Tiptap inline code mark extension excludes all other mark types from
    coexisting with itself. This makes sense for most mark types, but link
    is an exception.
    
    Unfortunately there's no way to dynamically create a list of all marks
    when `excludes` is initialized, as this happens when compiling the
    editor schema, so the latter is not available yet.
    
    Thus to fix the bug while keeping the side effects small, we have to
    explicitely list all mark types except link and code in `excludes` in
    the code mark definition.
    
    Fixes: #4900
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/marks/index.js
+++ src/marks/index.js
@@ -4,6 +4,7 @@
  */
 
 import TipTapItalic from '@tiptap/extension-italic'
+import Code from './Code.ts'
 import Link from './Link.js'
 import Strike from './Strike.js'
 import Strong from './Strong.js'
@@ -13,4 +14,4 @@ const Italic = TipTapItalic.extend({
 	name: 'em',
 })
 
-export { Italic, Link, Strike, Strong, Underline }
+export { Code, Italic, Link, Strike, Strong, Underline }
