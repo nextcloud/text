4d77e0120bfe04d89ed78d002016a98378c44651
Jonas <jonas@freesources.org>
2026-05-11 09:03:42 +0200

    fix(orderdList): preserve non-1 start numbers
    Also fixes preserving `0` as first list number.
    
    Fixes: #4828
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/nodes/OrderedList.ts
+++ src/nodes/OrderedList.ts
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import type { Node } from '@tiptap/pm/model'
+import type { MarkdownSerializerState } from 'prosemirror-markdown'
+
 import { OrderedList as TiptapOrderedList } from '@tiptap/extension-list'
 import { toggleListCommand } from '../commands'
 
@@ -22,6 +25,18 @@ const OrderedList = TiptapOrderedList.extend({
 			...this.parent?.(),
 			toggleOrderedList: toggleListCommand('orderedList', 'listItem'),
 		}
+	},
+
+	// Overwrite toMarkdown from prosemirror-markdown to preserve non-1 start numbers in lists
+	// @ts-expect-error - toMarkdown is a custom field not part of the official Tiptap API
+	toMarkdown(state: MarkdownSerializerState, node: Node) {
+		const start = node.attrs.start ?? 1
+		const maxW = String(start + node.childCount - 1).length
+		const space = state.repeat(' ', maxW + 2)
+		state.renderList(node, space, (i) => {
+			const nStr = String(start + i)
+			return state.repeat(' ', maxW - nStr.length) + nStr + '. '
+		})
 	},
 })
 
