e2a0026c48d51f7ed8d1fd8a63c94d33edfb452a
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-05-13 08:08:46 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/PlainTextReader-BPxHIqRy.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{P as n}from"./Wrapper-C056gsIi.chunk.mjs";import{B as r}from"./BaseReader-BLU1MMAw.chunk.mjs";import{n as o}from"./NcCheckboxRadioSwitch-D4IV4zWy-C7R54kfi.chunk.mjs";const s={name:"PlainTextReader",components:{BaseReader:r},provide:{extensions:()=>[n]},props:{content:{type:String,required:!0}}};var a=function(){var e=this,t=e._self._c;return t("BaseReader",{attrs:{content:e.content}})},i=[],p=o(s,a,i,!1,null,null);const l=p.exports;export{l as P};
-//# sourceMappingURL=PlainTextReader-BPxHIqRy.chunk.mjs.map
--- js/channel-sb44ACJU.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{U as o,C as r}from"./mermaid.core-Dl08ULFn.chunk.mjs";const e=(s,t)=>o.lang.round(r.parse(s)[t]);export{e as c};
-//# sourceMappingURL=channel-sb44ACJU.chunk.mjs.map
