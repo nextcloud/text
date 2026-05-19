967d50e6bb4945dacb66ca3e740e6fdb7c2aec8f
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-05-19 02:17:54 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/RichTextReader-BdhkEYob.chunk.mjs
+++ js/RichTextReader-BdhkEYob.chunk.mjs
@@ -1,2 +1,2 @@
 import{R as o}from"./Wrapper-DQAn0Nn9.chunk.mjs";import{B as i}from"./BaseReader-G07HnJAZ.chunk.mjs";import{n as s}from"./NcCheckboxRadioSwitch-D4IV4zWy-C7R54kfi.chunk.mjs";const c={name:"RichTextReader",components:{BaseReader:i},provide:{extensions(){return[o.configure({editing:!1})]}},props:{content:{type:String,required:!0}}};var a=function(){var e=this,t=e._self._c;return t("BaseReader",{attrs:{content:e.content},on:{"click-link":(n,r)=>e.$emit("click-link",n,r)}})},p=[],m=s(c,a,p,!1,null,null);const u=m.exports;export{u as R};
-//# sourceMappingURL=RichTextReader-B1laZUoj.chunk.mjs.map
+//# sourceMappingURL=RichTextReader-BdhkEYob.chunk.mjs.map
