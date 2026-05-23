c27d6acd6c1b82dc689d0bf7a13ff0ce4c22c0e3
Max <max@nextcloud.com>
2026-05-23 14:58:27 +0200

    chore(lint): remove outdated eslint disable lines
    Signed-off-by: Max <max@nextcloud.com>

--- src/viewer.js
+++ src/viewer.js
@@ -5,7 +5,6 @@ import { openMimetypesMarkdown, openMimetypesPlainText } from './helpers/mime.js
  * SPDX-FileCopyrightText: 2019 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-// eslint-disable-next-line import/no-unresolved, n/no-missing-import
 import 'vite/modulepreload-polyfill'
 
 /**
