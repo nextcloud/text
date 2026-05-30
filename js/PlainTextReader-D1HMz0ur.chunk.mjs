9897141abf7d3bc8916f0a689b1f0d7b4d0bf22a
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-05-30 09:33:34 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/PlainTextReader-D1HMz0ur.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{P as n}from"./Wrapper-CdCwTFI7.chunk.mjs";import{B as r}from"./BaseReader-EIxuYmPW.chunk.mjs";import{n as o}from"./NcCheckboxRadioSwitch-D4IV4zWy-C7R54kfi.chunk.mjs";const s={name:"PlainTextReader",components:{BaseReader:r},provide:{extensions:()=>[n]},props:{content:{type:String,required:!0}}};var a=function(){var e=this,t=e._self._c;return t("BaseReader",{attrs:{content:e.content}})},i=[],p=o(s,a,i,!1,null,null);const l=p.exports;export{l as P};
-//# sourceMappingURL=PlainTextReader-D1HMz0ur.chunk.mjs.map
--- js/channel-CFTc-taS.chunk.mjs
+++
@@ -1,2 +0,0 @@
-import{U as o,C as r}from"./mermaid.core-Chbz-xv4.chunk.mjs";const e=(s,t)=>o.lang.round(r.parse(s)[t]);export{e as c};
-//# sourceMappingURL=channel-CFTc-taS.chunk.mjs.map
