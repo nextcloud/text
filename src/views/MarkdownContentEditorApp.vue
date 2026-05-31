6a35495a13931d649a042fd952a14a6c86df9b57
Max <max@nextcloud.com>
2026-05-31 21:13:37 +0200

    chore(type): and split editor.js
    Introduce `createCollaborativeEditor` and `createMarkdownContentEditor`
    to superseed `createEditor` with more specific type signatures.
    
    Signed-off-by: Max <max@nextcloud.com>

---
+++ src/views/MarkdownContentEditorApp.vue
@@ -0,0 +1,53 @@
+<!--
+  - SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
+<template>
+	<MarkdownContentEditor
+		ref="editor-container"
+		:content
+		:fileId
+		:readOnly
+		:relativePath="filePath"
+		:shareToken
+		onReady="emit('ready')"
+		@create:content="(c: { markdown: string }) => emit('create:content', c)"
+		@update:content="(c: { markdown: string }) => emit('update:content', c)">
+		<template v-if="readonlyBarComponent" #readonlyBar>
+			<component :is="readonlyBarComponent" v-bind="readonlyBarProps" />
+		</template>
+	</MarkdownContentEditor>
+</template>
+
+<script setup lang='ts'>
+import { defineExpose, useTemplateRef } from 'vue'
+import MarkdownContentEditor from '../components/Editor/MarkdownContentEditor.vue'
+
+const {
+	fileId = undefined,
+	filePath = undefined,
+	readOnly = false,
+	readonlyBarComponent = undefined,
+	readonlyBarProps = {},
+	shareToken = undefined,
+} = defineProps<{
+	fileId?: number
+	filePath?: string
+	content: string
+	readOnly?: boolean
+	readonlyBarComponent?: string
+	readonlyBarProps?: object
+	shareToken?: string
+}>()
+
+const emit = defineEmits<{
+	(e: 'ready'): void
+	(e: 'create:content', content: { markdown: string }): void
+	(e: 'update:content', content: { markdown: string }): void
+}>()
+
+const editorContainer = useTemplateRef('editor-container')
+defineExpose({ editorContainer })
+
+</script>
