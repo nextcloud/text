fe73cfc37452588640c48d47fe6233b8bf90417a
Maksim Sukharev <antreesy.web@gmail.com>
2026-06-02 12:28:56 +0200

    fix(Editor): emit modified fileNode size on save
    Signed-off-by: Maksim Sukharev <antreesy.web@gmail.com>

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -635,6 +635,7 @@ export default defineComponent({
 		onSave() {
 			if (this.fileNode) {
 				this.fileNode.mtime = new Date()
+				this.fileNode.size = new Blob([this.serialize()]).size
 				emit('files:node:updated', this.fileNode)
 			}
 			this.$nextTick(() => {
