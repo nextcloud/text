df5b22953e533ff8a1aff67c0b3ef093aa363b25
Max <max@nextcloud.com>
2026-06-04 11:38:50 +0200

    chore(type): fix vue-tsc
    Signed-off-by: Max <max@nextcloud.com>

--- src/event-bus.d.ts
+++ src/event-bus.d.ts
@@ -8,7 +8,7 @@ declare module '@nextcloud/event-bus' {
 		'text:editor:attachments:updated': { attachmentSrcs: sting[] }
 		'text:editor:full-width': { value: boolean }
 		'text:editor:search-results': { totalMatches: number, matchIndex: number }
-		'text:toc:toggle': { visible: boolean } | void
+		'text:toc:toggle': { visible: boolean } | undefined
 		'text:toc:toggled': boolean
 		'text:toc:pin': { fileId: number, keep: boolean }
 	}
