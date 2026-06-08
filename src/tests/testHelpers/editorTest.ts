59e04f7f1bc78825ef2753b06454dd3ca7fc4152
Max <max@nextcloud.com>
2026-06-08 21:06:10 +0200

    chore(tests): use custom editor for Mathematics spec
    Also introduce `testEditor` test helper.
    
    Signed-off-by: Max <max@nextcloud.com>

---
+++ src/tests/testHelpers/editorTest.ts
@@ -0,0 +1,33 @@
+import type { Editor, Extensions } from '@tiptap/core'
+
+import { test as baseTest } from 'vitest'
+import { createMarkdownSerializer } from '../../extensions/Markdown.js'
+import markdownit from '../../markdownit/index.js'
+import createCustomEditor from './createCustomEditor.ts'
+
+export default baseTest.extend<{
+	editor: Editor
+	extensions: Extensions
+	markdownThroughEditor: (input: string) => string
+	serializeMarkdown: () => string
+}>({
+	editor: async ({ extensions }, use) => {
+		const editor = createCustomEditor('', extensions)
+		await use(editor)
+		editor.destroy()
+	},
+	extensions: [],
+	markdownThroughEditor: async ({ editor, serializeMarkdown }, use) => {
+		use((markdown: string) => {
+			const content = markdownit.render(markdown)
+			editor.commands.setContent(content)
+			return serializeMarkdown()
+		})
+	},
+	serializeMarkdown: async ({ editor }, use) => {
+		use(() => {
+			const serializer = createMarkdownSerializer(editor.schema)
+			return serializer.serialize(editor.state.doc)
+		})
+	},
+})
