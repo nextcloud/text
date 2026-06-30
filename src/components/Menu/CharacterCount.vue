54fc81831209e64401d24960e667455b8658860a
Jonas <jonas@freesources.org>
2026-06-30 15:34:37 +0200

    fix(menu): bring back width toggle and character count
    Both also need the workaround to be called 'NcActions*' in order to make
    NcActions see them.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/components/Menu/CharacterCount.vue
+++ src/components/Menu/CharacterCount.vue
@@ -4,30 +4,37 @@
 -->
 
 <template>
-	<NcActionText data-text-action-entry="character-count" :name="countString">
+	<NextcloudVueNcActionText data-text-action-entry="character-count" :name="countString">
 		<template #icon>
 			<AlphabeticalVariant />
 		</template>
-	</NcActionText>
+	</NextcloudVueNcActionText>
 </template>
 
 <script>
 import { translatePlural as n } from '@nextcloud/l10n'
 import { defineComponent, ref } from 'vue'
-import NcActionText from '@nextcloud/vue/components/NcActionText'
+import NextcloudVueNcActionText from '@nextcloud/vue/components/NcActionText'
 import { useEditor } from '../../composables/useEditor.ts'
 import { logger } from '../../helpers/logger.ts'
 import { AlphabeticalVariant } from '../icons.js'
 
 export default defineComponent({
-	name: 'CharacterCount',
+	// This component is used as a direct child of NcActions.
+	// Even if it actually renders NcActionButton, NcActions cannot see it due to rendering limitations in Vue.
+	// Though it works in general, NcActions doesn't handle it correctly. See NcActions docs for details.
+	// Hotfix - rename the component to NcActionButton because it represents and renders it.
+	name: 'NcActionText',
 	components: {
 		AlphabeticalVariant,
-		NcActionText,
+		NextcloudVueNcActionText,
 	},
 
 	props: {
-		visible: Boolean,
+		visible: {
+			type: Boolean,
+			default: false,
+		},
 	},
 
 	setup() {
