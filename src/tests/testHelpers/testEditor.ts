5983966d94b62678f45cc83c63bbe8fa64ff6a82
Jonas <jonas@freesources.org>
2026-06-30 17:23:38 +0200

    chore: add missing REUSE headers
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/tests/testHelpers/testEditor.ts
+++ src/tests/testHelpers/testEditor.ts
@@ -1,3 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 import type { Extensions } from '@tiptap/core'
 import type { Doc } from 'yjs'
 
