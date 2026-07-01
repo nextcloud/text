c72a8d07a68dbefa2c98e948313a26302811cc56
Jonas <jonas@freesources.org>
2026-07-01 15:02:56 +0200

    fix(ViewerView): set `inheritAttrs: false` for ViewerView
    Prevents attributes from Viewer component being used as HTML element
    attributes.
    
    Also simplify createApp ViewerComponent call.
    
    Signed-off-by: Jonas <jonas@freesources.org>

--- src/views/ViewerView.js
+++ src/views/ViewerView.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-import { createApp, defineComponent, h } from 'vue'
+import { createApp, defineComponent } from 'vue'
 import ViewerComponent from '../components/ViewerComponent.vue'
 
 // The vue instance used inside text constructed with the import above.
@@ -22,13 +22,12 @@ export default defineComponent({
 	name: 'ViewerView',
 	render: (h) => h('div', { style: { display: 'contents' } }),
 	props: ViewerComponent.props,
+	inheritAttrs: false,
 	mounted() {
-		innerApp = createApp({
-			render: () => h(ViewerComponent, {
-				...this.$props,
-				...this.$attrs,
-				onLoadedHandler: () => this.$emit('update:loaded', true),
-			}),
+		innerApp = createApp(ViewerComponent, {
+			...this.$props,
+			...this.$attrs,
+			onLoadedHandler: () => this.$emit('update:loaded', true),
 		})
 		innerApp.mount(this.$el)
 	},
