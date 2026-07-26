b3d7743343c20e12804a4221c89cf36c54436b93
Max <max@nextcloud.com>
2026-07-26 19:01:16 +0200

    fix(autosave): better dirty tracking with versions
    Keep track of the version that has our changes
    and compare it to the last saved version on the server.
    
    This allows fixing two scenarios:
    * Server response happily to save but does not actually save.
      The server will only save new versions every 10 seconds.
      If the latest save just happened it will still respond with 200
      but list the outdated version in the response.
      Comparing the versions shows that our changes have not been saved yet.
    * Other user already saved the file.
      our changes.
      So far dirty would stay true until WE save our changes.
      Comparing the versions also shows the file was saved
      when it was saved by someone else.
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/components/CollaborativeEditor.vue
+++ src/components/CollaborativeEditor.vue
@@ -286,7 +286,7 @@ export default defineComponent({
 			getBaseVersionEtag,
 			setBaseVersionEtag,
 		)
-		const { document, syncService } = provideSyncService(connection, openConnection)
+		const { document, syncService } = provideSyncService(connection, openConnection, setDirty)
 		const extensions = [
 			Autofocus.configure({ fileId: props.fileId }),
 			Collaboration.configure({ document: ydoc }),
@@ -320,6 +320,7 @@ export default defineComponent({
 
 		const { saveService } = provideSaveService(
 			connection,
+			document,
 			syncService,
 			serialize,
 			ydoc,
@@ -591,6 +592,7 @@ export default defineComponent({
 			bus.on('idle', this.onIdle)
 			bus.on('save', this.onSave)
 			bus.on('permissionChange', this.onPermissionChange)
+			bus.on('changesPushed', this.onChangesPushed)
 		},
 
 		unlistenSyncServiceEvents() {
@@ -603,6 +605,7 @@ export default defineComponent({
 			bus.off('idle', this.onIdle)
 			bus.off('save', this.onSave)
 			bus.off('permissionChange', this.onPermissionChange)
+			bus.off('changesPushed', this.onChangesPushed)
 		},
 
 		reconnect() {
@@ -752,17 +755,6 @@ export default defineComponent({
 				}
 				this.$emit('ready')
 			}
-			if (Object.hasOwn(state, 'dirty')) {
-				if (state.dirty) {
-					// ignore initial loading and other automated changes before first user change
-					if (this.editor.can().undo() || this.editor.can().redo()) {
-						this.setDirty(state.dirty)
-						this.saveService.autosave()
-					}
-				} else {
-					this.setDirty(state.dirty)
-				}
-			}
 		},
 
 		onIdle() {
@@ -800,6 +792,10 @@ export default defineComponent({
 			} else {
 				showWarning(t('text', 'You now have edit permissions for this document.'))
 			}
+		},
+
+		onChangesPushed() {
+			this.saveService.autosave()
 		},
 
 		onFocus() {
