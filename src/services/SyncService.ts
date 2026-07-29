dfccfab1846c81a33303df19f88845e34ea53bec
Max <max@nextcloud.com>
2026-07-29 10:11:40 +0200

    chore(tests): fix autosave test with new timing
    Signed-off-by: Max <max@nextcloud.com>

--- src/services/SyncService.ts
+++ src/services/SyncService.ts
@@ -259,6 +259,7 @@ class SyncService {
 					if (hadUpdate) {
 						// this.version has been increased in receiveSteps
 						this.bus.emit('changesPushed', { version: this.version })
+						logger.debug('changesPushed', { version: this.version })
 					}
 				}
 			})
