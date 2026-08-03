d0eadcc0ef582413485897758e74cbcbf314f70b
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-08-03 12:34:48 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/swimlanesDiagram-G3AALYLV-DFwJ3iDx.chunk.mjs
+++
@@ -1,9 +0,0 @@
-import{c as r,s as o}from"./flowDiagram-23GEKE2U-CxWc3iDj.chunk.mjs";import{_ as e}from"./mermaid.core-mgzV_kmP.chunk.mjs";import"./chunk-5VM5RSS4-DHd_xJsu.chunk.mjs";import"./chunk-XXDRQBXY-BRJN-cdC.chunk.mjs";import"./chunk-VR4S4FIN-DPQp-3Pz.chunk.mjs";import"./chunk-32BRIVSS-C3xf9tpy.chunk.mjs";import"./index-C_S1vBwF.chunk.mjs";import"./emoji-picker-DWqmQ038.chunk.mjs";import"./index-gqx3AnrG.chunk.mjs";import"./channel-DfhcO3BN.chunk.mjs";import"./modulepreload-polyfill-CBWYtosv.chunk.mjs";var m=e(t=>`${o(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),i=m,k=r({defaultLayout:"swimlane",styles:i});export{k as diagram};
-//# sourceMappingURL=swimlanesDiagram-G3AALYLV-DFwJ3iDx.chunk.mjs.map
