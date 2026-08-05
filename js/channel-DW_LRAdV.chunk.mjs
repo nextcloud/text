b26f80a6bf1b4bf45a5f97ed7121ae7009868516
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-08-05 16:59:37 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/channel-DW_LRAdV.chunk.mjs
@@ -0,0 +1,2 @@
+import{U as o,C as r}from"./mermaid.core-Dni-UI6z.chunk.mjs";const e=(s,t)=>o.lang.round(r.parse(s)[t]);export{e as c};
+//# sourceMappingURL=channel-DW_LRAdV.chunk.mjs.map
---
+++ js/swimlanesDiagram-G3AALYLV-De_kzOL1.chunk.mjs
@@ -0,0 +1,9 @@
+import{c as r,s as o}from"./flowDiagram-23GEKE2U-D28Wz1NG.chunk.mjs";import{_ as e}from"./mermaid.core-Dni-UI6z.chunk.mjs";import"./chunk-5VM5RSS4-DTQcISUf.chunk.mjs";import"./chunk-XXDRQBXY-BGGQue_I.chunk.mjs";import"./chunk-VR4S4FIN-Cs0Zkq-y.chunk.mjs";import"./chunk-32BRIVSS-BuoVEotw.chunk.mjs";import"./index-C_S1vBwF.chunk.mjs";import"./emoji-picker-DWqmQ038.chunk.mjs";import"./index-gqx3AnrG.chunk.mjs";import"./channel-DW_LRAdV.chunk.mjs";import"./modulepreload-polyfill-CBWYtosv.chunk.mjs";var m=e(t=>`${o(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),i=m,k=r({defaultLayout:"swimlane",styles:i});export{k as diagram};
+//# sourceMappingURL=swimlanesDiagram-G3AALYLV-De_kzOL1.chunk.mjs.map
