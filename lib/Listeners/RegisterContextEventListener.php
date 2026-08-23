2444467e62fa78905659ab627c69dbf829dd82b3
Max <max@nextcloud.com>
2026-08-23 09:17:49 +0200

    chore(refactor): register Context Factory instead of callable
    Signed-off-by: Max <max@nextcloud.com>

--- lib/Listeners/RegisterContextEventListener.php
+++ lib/Listeners/RegisterContextEventListener.php
@@ -12,18 +12,10 @@ use OCA\Text\Context\FileContextFactory;
 use OCA\Text\Event\RegisterContextEvent;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
-use OCP\Files\NotPermittedException;
-use OCP\IUserSession;
 use Override;
 
 /** @implements IEventListener<Event|RegisterContextEvent> */
 class RegisterContextEventListener implements IEventListener {
-
-	public function __construct(
-		private readonly FileContextFactory $fileContextFactory,
-		private readonly IUserSession $userSession,
-	) {
-	}
 
 	#[Override]
 	public function handle(Event $event): void {
@@ -33,17 +25,7 @@ class RegisterContextEventListener implements IEventListener {
 
 		$event->getContextManager()->registerContext(
 			'file',
-			function (int $id, string $type, ?string $shareToken) {
-				if ($shareToken === null) {
-					$user = $this->userSession->getUser();
-					if ($user === null) {
-						throw new NotPermittedException();
-					}
-					return $this->fileContextFactory->buildForUser($user, $id);
-				} else {
-					return $this->fileContextFactory->buildForShare($shareToken, $id);
-				}
-			}
+			FileContextFactory::class,
 		);
 	}
 }
