fd64069aa2117d4183e00607c7b8eedf08f8a025
Max <max@nextcloud.com>
2026-09-02 14:00:43 +0200

    chore(refactor): split classes in their own files
    Signed-off-by: Max <max@nextcloud.com>

--- lib/Context/IContext.php
+++ lib/Context/IContext.php
@@ -11,7 +11,6 @@ namespace OCA\Text\Context;
 use OCA\Text\Db\Document;
 use OCA\Text\Db\Session;
 use OCP\Files\File;
-use OCP\Files\Lock\ILock;
 use OCP\Files\NotFoundException;
 use OCP\Files\NotPermittedException;
 
@@ -38,61 +37,4 @@ interface IContext {
 	 * This will be called when the last active editing session ends.
 	 */
 	public function cleanup(): void;
-}
-
-readonly class DocumentData {
-	public function __construct(
-		public Document $document,
-		public ?string $documentState,
-	) {
-	}
-
-	public function jsonSerialize(): array {
-		return [
-			'document' => $this->document,
-			'documentState' => $this->documentState,
-		];
-	}
-}
-
-readonly class SessionInfo {
-	public function __construct(
-		public ?string $content,
-		public bool $readOnly,
-		public ?ILock $lock,
-		public bool $hasOwner,
-	) {
-	}
-
-	public function jsonSerialize(): array {
-		return [
-			'content' => $this->content,
-			'readOnly' => $this->readOnly,
-			'lock' => $this->lock,
-			'hasOwner' => $this->hasOwner,
-		];
-	}
-}
-
-readonly class NewSessionData {
-	public function __construct(
-		public DocumentData $documentData,
-		public SessionInfo $sessionInfo,
-		public Session $session,
-		public ?string $displayName,
-	) {
-	}
-
-	public function jsonSerialize(): array {
-		return array_merge(
-			$this->documentData->jsonSerialize(),
-			$this->sessionInfo->jsonSerialize(),
-			[
-				'session' => array_merge(
-					$this->session->jsonSerialize(),
-					['displayName' => $this->displayName],
-				),
-			],
-		);
-	}
 }
