d7d37b91ab502e9ec2bfd54fb502b7c9353e9d41
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-09-16 10:22:58 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

---
+++ js/chunk-XXDRQBXY-E69a9qGQ.chunk.mjs
@@ -0,0 +1,2 @@
+import{_ as a,j as t}from"./mermaid.core-DK3sk0uA.chunk.mjs";var r=a((e,n)=>{let o;return n==="sandbox"&&(o=t("#i"+e)),(n==="sandbox"?t(o.nodes()[0].contentDocument.body):t("body")).select(`[id="${e}"]`)},"getDiagramElement");export{r as g};
+//# sourceMappingURL=chunk-XXDRQBXY-E69a9qGQ.chunk.mjs.map
