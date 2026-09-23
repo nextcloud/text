57a8269925eaff0de53de8d9955f1bccd9fdb0d2
nextcloud-command <nextcloud-command@users.noreply.github.com>
2026-09-23 13:17:15 +0000

    chore(assets): recompile assets
    Signed-off-by: nextcloud-command <nextcloud-command@users.noreply.github.com>

--- js/swimlanesDiagram-VR7AAH4N-Da9AiKGV.chunk.mjs
+++
@@ -1,9 +0,0 @@
-import{c as r,s as o}from"./flowDiagram-HODETNUW-CaMWRng_.chunk.mjs";import{_ as e}from"./mermaid.core-mfaILIwr.chunk.mjs";import"./chunk-5VM5RSS4-C2hFsYNv.chunk.mjs";import"./chunk-XXDRQBXY-CrH9_1WT.chunk.mjs";import"./chunk-POPQ4Y6H-ChblU3sI.chunk.mjs";import"./chunk-F27PBJKO-7SHuxu6Y.chunk.mjs";import"./translation-DoG5ZELJ-CIdMpLn_.chunk.mjs";import"./emoji-picker-DVpyXNhW.chunk.mjs";import"./channel-C9I4DaML.chunk.mjs";import"./modulepreload-polyfill-CBWYtosv.chunk.mjs";var m=e(t=>`${o(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),i=m,g=r({defaultLayout:"swimlane",styles:i});export{g as diagram};
-//# sourceMappingURL=swimlanesDiagram-VR7AAH4N-Da9AiKGV.chunk.mjs.map
---
+++ js/swimlanesDiagram-VR7AAH4N-DtOKwqh0.chunk.mjs
@@ -0,0 +1,9 @@
+import{c as r,s as o}from"./flowDiagram-HODETNUW-Di2R1sFU.chunk.mjs";import{_ as e}from"./mermaid.core-BbIXSBNZ.chunk.mjs";import"./chunk-5VM5RSS4-BugHWb4z.chunk.mjs";import"./chunk-XXDRQBXY-7JJ3Q1wJ.chunk.mjs";import"./chunk-POPQ4Y6H-CkbDyJm7.chunk.mjs";import"./chunk-F27PBJKO-Bd6BPniV.chunk.mjs";import"./translation-DoG5ZELJ-CIdMpLn_.chunk.mjs";import"./emoji-picker-DVpyXNhW.chunk.mjs";import"./channel-OYhhg4aZ.chunk.mjs";import"./modulepreload-polyfill-CBWYtosv.chunk.mjs";var m=e(t=>`${o(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),i=m,g=r({defaultLayout:"swimlane",styles:i});export{g as diagram};
+//# sourceMappingURL=swimlanesDiagram-VR7AAH4N-DtOKwqh0.chunk.mjs.map
